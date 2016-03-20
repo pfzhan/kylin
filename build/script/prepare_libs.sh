6cc2d04f68fdef8c23667e509616260bb9a9a5da
lidongsjtu <lidong@apache.org>
2016-03-20 23:40:24 +0800

    Add extension project for coprocessor

--- build/script/prepare_libs.sh
+++ build/script/prepare_libs.sh
@@ -10,10 +10,10 @@ echo "copy lib file"
 rm -rf build/lib
 mkdir build/lib
 cp kylin/assembly/target/kylin-assembly-${kylin_version}-job.jar build/lib/kylin-job-${kylin_version}.jar
-cp kylin/storage-hbase/target/kylin-storage-hbase-${kylin_version}-coprocessor.jar build/lib/kylin-coprocessor-${kylin_version}.jar
+cp extensions/storage-hbase/target/kap-storage-hbase-${kap_version}-coprocessor.jar build/lib/kap-coprocessor-${kap_version}.jar
 cp kylin/jdbc/target/kylin-jdbc-${kylin_version}.jar build/lib/kylin-jdbc-${kylin_version}.jar
 
 # Copied file becomes 000 for some env (e.g. Cygwin)
 chmod 644 build/lib/kylin-job-${kylin_version}.jar
-chmod 644 build/lib/kylin-coprocessor-${kylin_version}.jar
+chmod 644 build/lib/kap-coprocessor-${kap_version}.jar
 chmod 644 build/lib/kylin-jdbc-${kylin_version}.jar
