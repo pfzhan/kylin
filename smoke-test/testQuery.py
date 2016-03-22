9aafeb8497bdfdd47ccb7d0fc557dd2fcdbd3f6f
lidongsjtu <lidong@apache.org>
2016-03-22 15:06:27 +0800

    Print more information on init service

--- smoke-test/testQuery.py
+++ smoke-test/testQuery.py
@@ -26,7 +26,7 @@ class testQuery(unittest.TestCase):
         for sql_file in sql_files:
             index += 1
             sql_statement = open(sql_file).read().strip()
-            payload = "{\"sql\": \"" + sql_statement + "\", \"offset\": 0, \"limit\": \"50000\",\"acceptPartial\":false, \"project\":\"learn_kylin\"}"
+            payload = "{\"sql\": \"" + sql_statement + "\", \"offset\": 0, \"limit\": \"50000\", \"acceptPartial\":false, \"project\":\"learn_kylin\"}"
             print 'Test Query #' + str(index) + ': \n' + sql_statement
             response = requests.request("POST", url, data=payload, headers=headers)
 
