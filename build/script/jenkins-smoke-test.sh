0b98ea8fb86a169a6d23997ecdb3bbe030435ce5
lidongsjtu <lidong@apache.org>
2016-03-24 21:39:18 +0800

    Update jenkins smoke tests

--- build/script/jenkins-smoke-test.sh
+++ build/script/jenkins-smoke-test.sh
@@ -2,13 +2,13 @@
 pkg_name=`ls dist/*.tar.gz`
 
 test_home=smoke-test-${BUILD_TAG}
-mkdir smoke-test-${BUILD_TAG}
+mkdir ${test_home}
 
 # copy test scripts to test home
-cp -rf kylin/smoke-test/* smoke-test-${BUILD_TAG}/
+cp -rf kylin/build/smoke-test/* ${test_home}/
 
 # run test scripts
 bash ${test_home}/smoke-test.sh ${pkg_name} ${test_home}
 
 # clear test home
-rm -rf smoke-test-${BUILD_TAG}
+rm -rf ${test_home}
