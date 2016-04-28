403e7a8d16fabdae29e049a4dd6e29e0ce3fccc9
Yang Li <liyang@apache.org>
2016-04-28 19:29:07 +0800

    KYLIN-1636 Mmap read benchmark

--- extensions/core-cube/src/main/java/io/kyligence/kap/cube/index/GTColumnForwardIndex.java
+++ extensions/core-cube/src/main/java/io/kyligence/kap/cube/index/GTColumnForwardIndex.java
@@ -15,6 +15,7 @@ public class GTColumnForwardIndex implements IColumnForwardIndex {
     private final String idxFilename;
     private final int fixedBitsNum;
     private final String colName;
+    private boolean useMmap;
 
     public GTColumnForwardIndex(String colName, int maxValue, String idxFilename) {
         this.colName = colName;
@@ -40,6 +41,14 @@ public class GTColumnForwardIndex implements IColumnForwardIndex {
         }
     }
 
+    public void setUseMmap(boolean b) {
+        this.useMmap = b;
+    }
+
+    public void deleteIndexFile() {
+        new File(idxFilename).delete();
+    }
+
     private class GTColumnForwardIndexBuilder implements IColumnForwardIndex.Builder {
         final FixedBitSingleValueWriter writer;
         int rowCounter = 0;
@@ -63,7 +72,10 @@ public class GTColumnForwardIndex implements IColumnForwardIndex {
         FixedBitSingleValueReader reader;
 
         public GTColumnForwardIndexReader() throws IOException {
-            this.reader = FixedBitSingleValueReader.forHeap(new File(idxFilename), fixedBitsNum);
+            if (useMmap)
+                this.reader = FixedBitSingleValueReader.forMmap(new File(idxFilename), fixedBitsNum);
+            else
+                this.reader = FixedBitSingleValueReader.forHeap(new File(idxFilename), fixedBitsNum);
         }
 
         @Override
