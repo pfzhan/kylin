f8abd10b6cbc15fff4a2302a0ebdef017ac94d54
lidongsjtu <lidong@apache.org>
2016-06-23 13:03:46 +0800

    refine parquet ii rdd

--- extensions/storage-parquet-format/src/main/java/io/kyligence/kap/storage/parquet/format/pageIndex/format/ParquetPageIndexInputFormat.java
+++ extensions/storage-parquet-format/src/main/java/io/kyligence/kap/storage/parquet/format/pageIndex/format/ParquetPageIndexInputFormat.java
@@ -2,11 +2,18 @@ package io.kyligence.kap.storage.parquet.format.pageIndex.format;
 
 import java.io.IOException;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 
 public class ParquetPageIndexInputFormat<K, V> extends FileInputFormat<K, V> {
     public org.apache.hadoop.mapreduce.RecordReader<K, V> createRecordReader(org.apache.hadoop.mapreduce.InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
         return new ParquetPageIndexRecordReader<>();
+    }
+
+    @Override
+    protected boolean isSplitable(JobContext context, Path filename) {
+        return false;
     }
 }
--- extensions/storage-parquet/src/main/java/io/kyligence/kap/storage/parquet/spark/input/ParquetPageIndexSparkCLI.java
+++ extensions/storage-parquet/src/main/java/io/kyligence/kap/storage/parquet/spark/input/ParquetPageIndexSparkCLI.java
@@ -3,13 +3,20 @@ package io.kyligence.kap.storage.parquet.spark.input;
 import java.io.IOException;
 import java.util.List;
 
+import io.kyligence.kap.storage.parquet.format.pageIndex.ParquetPageIndexTable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.kylin.common.util.ByteArray;
 import org.apache.kylin.common.util.BytesUtil;
 import org.apache.kylin.gridtable.GTInfo;
+import org.apache.kylin.metadata.filter.ColumnTupleFilter;
+import org.apache.kylin.metadata.filter.CompareTupleFilter;
+import org.apache.kylin.metadata.filter.ConstantTupleFilter;
+import org.apache.kylin.metadata.filter.TupleFilter;
+import org.apache.kylin.metadata.model.ColumnDesc;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -24,7 +31,6 @@ import io.kyligence.kap.storage.parquet.format.serialize.SerializableImmutableRo
 import scala.Tuple2;
 
 public class ParquetPageIndexSparkCLI {
-    static GTInfo INFO;
     protected static final Logger logger = LoggerFactory.getLogger(ParquetPageIndexSparkCLI.class);
 
     final int dataSize = 50;
@@ -52,19 +58,6 @@ public class ParquetPageIndexSparkCLI {
             writer.close();
         }
     }
-
-    //    public GTScanRequest prepareRequest() throws IOException {
-    //        INFO = UnitTestSupport.hllInfo();
-    //        TupleFilter filter = new CompareTupleFilter(TupleFilter.FilterOperatorEnum.EQ);
-    //        filter.addChild(new ColumnTupleFilter(ColumnDesc.mockup(null, 1, "1", "int").getRef()));
-    //        byte[] buffer = new byte[columnLength[0]];
-    //        BytesUtil.writeUnsigned(data[0], buffer, 0, buffer.length);
-    //        filter.addChild(new ConstantTupleFilter(new ByteArray(buffer)));
-    //
-    //        GTScanRequest scanRequest = new GTScanRequest(INFO, null, new ImmutableBitSet(0, 3), new ImmutableBitSet(1, 3), new ImmutableBitSet(3, 6), new String[] { "SUM", "SUM", "COUNT_DISTINCT" }, filter, true, 0.5);
-    //        return scanRequest;
-    //    }
-
     public static void main(String args[]) throws IOException, ClassNotFoundException {
         SparkConf sparkConf = new SparkConf();
 //        sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
@@ -73,24 +66,36 @@ public class ParquetPageIndexSparkCLI {
 //                Class.forName("org.apache.hadoop.io.Text")
 //        });
         JavaSparkContext context = new JavaSparkContext(sparkConf);
+
+        // build index for test
         Configuration config = new Configuration();
         ParquetPageIndexSparkCLI app = new ParquetPageIndexSparkCLI();
         app.writeIndexFile(config);
-        //        GTScanRequest request = app.prepareRequest();
-        //        ByteBuffer buffer = ByteBuffer.allocate(BytesSerializer.SERIALIZE_BUFFER_SIZE);
-        //        GTScanRequest.serializer.serialize(request, buffer);
-        //        String requestStr = new String(buffer.array());
-        //        config.set(ParquetFormatConstants.KYLIN_SCAN_REQUEST, requestStr);
 
-        JavaPairRDD<Text, SerializableImmutableRoaringBitmap> rdd = context.newAPIHadoopFile("/tmp/index/*.inv", ParquetPageIndexInputFormat.class, Text.class, SerializableImmutableRoaringBitmap.class, config);
-        JavaRDD<Tuple2<String, SerializableImmutableRoaringBitmap>> rdd2 = rdd.map(new Function<Tuple2<Text, SerializableImmutableRoaringBitmap>, Tuple2<String, SerializableImmutableRoaringBitmap>>() {
+        JavaPairRDD<String, ParquetPageIndexTable> rdd = context.newAPIHadoopFile("/tmp/index/*.inv", ParquetPageIndexInputFormat.class, String.class, ParquetPageIndexTable.class, config);
+        JavaRDD<Tuple2<String, SerializableImmutableRoaringBitmap>> rdd2 = rdd.map(new Function<Tuple2<String, ParquetPageIndexTable>, Tuple2<String, SerializableImmutableRoaringBitmap>>() {
             @Override
-            public Tuple2<String, SerializableImmutableRoaringBitmap> call(Tuple2<Text, SerializableImmutableRoaringBitmap> tuple) throws Exception {
-                logger.info("Key: " + tuple._1().toString());
-                logger.info("Value: " + tuple._2().getBitmap().toString());
-                return new Tuple2<>(tuple._1.toString(), tuple._2);
+            public Tuple2<String, SerializableImmutableRoaringBitmap> call(Tuple2<String, ParquetPageIndexTable> tuple) throws Exception {
+                // build test filter, will get from broadcast in real case
+                int dataSize = 50;
+                int columnLength = Integer.SIZE - Integer.numberOfLeadingZeros(dataSize);
+                TupleFilter filter = new CompareTupleFilter(TupleFilter.FilterOperatorEnum.EQ);
+                filter.addChild(new ColumnTupleFilter(ColumnDesc.mockup(null, 1, "1", null).getRef()));
+                byte[] buffer = new byte[columnLength];
+                BytesUtil.writeUnsigned(10, buffer, 0, buffer.length);
+                filter.addChild(new ConstantTupleFilter(new ByteArray(buffer)));
+
+                // lookup index table
+                SerializableImmutableRoaringBitmap result = new SerializableImmutableRoaringBitmap(tuple._2().lookup(filter));
+
+                logger.info("InMap: Key: " + tuple._1());
+                logger.info("InMap: Value: " + result);
+
+                return new Tuple2<>(tuple._1(), result);
             }
         });
+
+        // print results
         List<Tuple2<String, SerializableImmutableRoaringBitmap>> result = rdd2.collect();
         for (Tuple2<String, SerializableImmutableRoaringBitmap> tuple : result) {
             logger.info("result: key: " + tuple._1);
