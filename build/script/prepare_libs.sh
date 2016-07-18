c565450b28e63ab6eb009d8c3ec5f0e8fb63cc2e
Hongbin Ma <mahongbin@apache.org>
2016-07-18 19:24:55 +0800

    Revert "fix package"
    This reverts commit a92869a830504193fe7ba7c1882c5e5e94a0104d.

--- build/script/prepare_libs.sh
+++ build/script/prepare_libs.sh
@@ -12,7 +12,7 @@ mkdir build/lib build/tool
 cp extensions/assembly/target/kap-assembly-${kap_version}-job.jar build/lib/kylin-job-kap-${release_version}.jar
 cp extensions/storage-hbase/target/kap-storage-hbase-${kap_version}-coprocessor.jar build/lib/kylin-coprocessor-kap-${release_version}.jar
 cp extensions/tool/target/kap-tool-*-assembly.jar build/tool/kap-tool-${release_version}.jar
-cp extensions/storage-parquet/target/kap-storage-parquet-*-spark.jar build/lib/kylin-storage-parquet-kap-${release_version}.jar
+cp extensions/storage-parquet/target/kap-storage-parquet-${kap_version}-spark.jar build/lib/kylin-storage-parquet-kap-${kap_version}.jar
 cp kylin/jdbc/target/kylin-jdbc-${kylin_version}.jar build/lib/kylin-jdbc-kap-${release_version}.jar
 
 
@@ -20,5 +20,5 @@ cp kylin/jdbc/target/kylin-jdbc-${kylin_version}.jar build/lib/kylin-jdbc-kap-${
 chmod 644 build/lib/kylin-job-kap-${release_version}.jar
 chmod 644 build/lib/kylin-coprocessor-kap-${release_version}.jar
 chmod 644 build/lib/kylin-jdbc-kap-${release_version}.jar
-chmod 644 build/lib/kylin-storage-parquet-kap-${release_version}.jar
+chmod 644 build/lib/kylin-storage-parquet-kap-${kap_version}.jar
 chmod 644 build/tool/kap-tool-${release_version}.jar
--- build/script/release.sh
+++ build/script/release.sh
@@ -4,5 +4,4 @@ export release_version=2.0
 dir=$(dirname ${0})
 cd ${dir}/../..
 
-echo "KAP Release Version: ${release_version}"
 sh build/script/package.sh
