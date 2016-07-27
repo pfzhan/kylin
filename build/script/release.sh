4cc4b7c49ecbaf3ac35377ef04144894e5e420d3
lidongsjtu <lidong@apache.org>
2016-07-27 20:11:49 +0800

    release script

--- build/script/release.sh
+++ build/script/release.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-export release_version=2.0
+
+# set release version, default value is from pom.xml
+export release_version=2.1-staging
 
 dir=$(dirname ${0})
 cd ${dir}/../..
 
 echo "KAP Release Version: ${release_version}"
 sh build/script/package.sh
+
+if [ -f *.license ]; then
+    kap_dir=`tar -tf dist/kylin-kap-*-obf.tar.gz|head -1`
+    rm -rf $kap_dir
+    mkdir $kap_dir
+    cp *.license $kap_dir
+    gzip -d dist/kylin-kap-*-obf.tar.gz
+    tar -uf dist/kylin-kap-*-obf.tar $kap_dir/*.license
+    gzip dist/kylin-kap-*-obf.tar
+    rm -rf $kap_dir
+fi
