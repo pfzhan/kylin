1218b751ae1a789c95b2039f7905e75c590de867
Roger Shi <roger@kyligence.io>
2016-08-25 16:03:35 +0800

    #69 tool to print raw table parquet file

---
+++ extensions/tool/src/main/java/io/kyligence/kap/tool/storage/RawTableParquetFilePrinter.java
@@ -0,0 +1,67 @@
+package io.kyligence.kap.tool.storage;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.kylin.dimension.IntegerDimEnc;
+import org.apache.kylin.metadata.datatype.BigDecimalSerializer;
+import org.apache.kylin.metadata.datatype.DataType;
+import org.apache.kylin.metadata.datatype.DataTypeSerializer;
+import org.apache.kylin.metadata.datatype.DateTimeSerializer;
+import org.apache.kylin.metadata.datatype.LongSerializer;
+import org.apache.kylin.metadata.datatype.StringSerializer;
+
+import io.kyligence.kap.storage.parquet.format.file.ParquetBundleReader;
+import io.kyligence.kap.storage.parquet.format.file.ParquetBundleReaderBuilder;
+import org.apache.parquet.io.api.Binary;
+
+/**
+ * This tool is used to print raw table parquet file
+ */
+public class RawTableParquetFilePrinter {
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            return;
+        }
+        String path = args[0];
+        PrintFile(path);
+    }
+    public static void PrintFile(String path) throws IOException {
+
+        DateTimeSerializer dateSe = new DateTimeSerializer(null);
+        StringSerializer stringSe = new StringSerializer(new DataType("A", 1, 1));
+        LongSerializer longSe = new LongSerializer(null);
+        IntegerDimEnc.IntegerSerializer intSe = (IntegerDimEnc.IntegerSerializer) new IntegerDimEnc().asDataTypeSerializer();
+        BigDecimalSerializer bigdecSe = new BigDecimalSerializer(new DataType("B", 1, 1));
+
+        DataTypeSerializer[] ses = new DataTypeSerializer[] { dateSe, dateSe, dateSe, stringSe, stringSe, stringSe, stringSe, stringSe, stringSe, stringSe, stringSe, longSe, bigdecSe, longSe, longSe, intSe, longSe, intSe };
+
+        ParquetBundleReader bundleReader = new ParquetBundleReaderBuilder().setPath(new Path(path)).setConf(new Configuration()).build();
+        while (true) {
+            List<Object> data = bundleReader.read();
+            if (data == null) {
+                break;
+            }
+
+            for (int i = 0; i < data.size(); i++) {
+                Binary d = (Binary) data.get(i);
+                if (i < ses.length && ses[i] != null) {
+                    des(d, ses[i]);
+                } else {
+                    System.out.print(new String(d.getBytes()) + "\t");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    private static void des(Binary binary, DataTypeSerializer<?> se) {
+        byte[] b = binary.getBytes();
+        System.out.print(se.deserialize(ByteBuffer.wrap(b)) + ",\t");
+    }
+
+}
