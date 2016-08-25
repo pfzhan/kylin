790482df930414ef910280adb032b014c20ff0e3
Roger Shi <roger@kyligence.io>
2016-08-25 13:18:48 +0800

    #69 pull out hash function, be reused in query

--- extensions/storage-parquet/src/main/java/io/kyligence/kap/storage/parquet/steps/RawTablePageIndexMapper.java
+++ extensions/storage-parquet/src/main/java/io/kyligence/kap/storage/parquet/steps/RawTablePageIndexMapper.java
@@ -110,40 +110,13 @@ public class RawTablePageIndexMapper extends KylinMapper<ByteArrayListWritable, 
         if (counter % BatchConstants.NORMAL_RECORD_LOG_THRESHOLD == 0) {
             logger.info("Handled " + counter + " records!");
         }
-        List<byte[]> hashedValue = hash(key.get());
+        List<byte[]> hashedValue = RawTableUtils.hash(key.get());
         indexBundleWriter.write(hashedValue, value.get());
     }
 
     @Override
     protected void cleanup(Context context) throws IOException, InterruptedException {
         indexBundleWriter.close();
-    }
-
-    private List<byte[]> hash(List<byte[]> value) {
-        List<byte[]> result = new ArrayList<>(value.size());
-        for (byte[] v: value) {
-            result.add(hash(v));
-        }
-        return result;
-    }
-
-    private byte[] hash(byte[] value) {
-        byte[] result = new byte[8];
-
-        if (value.length <= 8) {
-            System.arraycopy(value, 0, result, 0, value.length);
-            for (int i = value.length; i < 8; i++) {
-                result[i] = (byte)0;
-            }
-        } else {
-            System.arraycopy(value, 0, result, 0, 8);
-
-            for (int i = 8; i < value.length; i++) {
-                result[i % 8] ^= value[i];
-            }
-        }
-
-        return result;
     }
 
     private List<Integer> getExtendedColumnLen() {
