cd18dd40ad7f65f068ab9739a0bbe9441115627c
lidongsjtu <lidong@apache.org>
2016-09-01 21:23:20 +0800

    minor, fix slow test

--- extensions/storage-parquet-format/src/test/java/io/kyligence/kap/storage/parquet/format/pageIndex/IndexMapCacheTest.java
+++ extensions/storage-parquet-format/src/test/java/io/kyligence/kap/storage/parquet/format/pageIndex/IndexMapCacheTest.java
@@ -2,12 +2,16 @@ package io.kyligence.kap.storage.parquet.format.pageIndex;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.apache.kylin.common.util.ByteArray;
 import org.apache.kylin.common.util.BytesUtil;
 import org.apache.kylin.common.util.Pair;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
 
 import io.kyligence.kap.common.util.LocalFileMetadataTestCase;
 import io.kyligence.kap.storage.parquet.format.pageIndex.column.IndexMapCache;
@@ -39,15 +43,25 @@ public class IndexMapCacheTest extends LocalFileMetadataTestCase {
         System.clearProperty("kap.storage.columnar.ii.spill.threshold.mb");
     }
 
+    private void createGarbage() {
+        byte[] garbage = new byte[512 * 1024 * 1024];
+        for (byte g : garbage) {
+            g = 1;
+        }
+    }
+
     private void testIndexMapCache() {
-        int dataSize = 10;
+        int dataSize = 2;
         int columnLength = 8;
-        int pageNum = 10;
+        int pageNum = 2;
 
         IndexMapCache indexMapCache = new IndexMapCache("test", true, new IntEncoding(), new MutableRoaringBitmapEncoding(), true);
         for (int c = 0; c < 3; c++) {
             for (int i = 0; i < dataSize; i++) {
                 for (int j = 0; j < pageNum; j++) {
+                    // create garbage to bring progress after gc.
+                    createGarbage();
+
                     ByteArray key = ByteArray.allocate(columnLength);
                     BytesUtil.writeUnsigned(i, key.array(), key.offset(), key.length());
                     indexMapCache.put(key, j);
@@ -59,6 +73,9 @@ public class IndexMapCacheTest extends LocalFileMetadataTestCase {
         int num = 0;
         for (Pair<Comparable, ? extends Iterable<? extends Number>> val : indexMapCache.getIterable(true)) {
             assertEquals(num++, val.getFirst());
+            List<? extends Number> vals = Lists.newArrayList(val.getSecond());
+            assertEquals(0, vals.get(0));
+            assertEquals(1, vals.get(1));
         }
 
         assertEquals(num, dataSize);
