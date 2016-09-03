cee8122413149789dec6fa200188535c64bffc9e
lidongsjtu <lidong@apache.org>
2016-09-03 15:31:48 +0800

    #83 scripts for smoke test and releases

--- build/script/release.sh
+++ build/script/release.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # set release version, default value is from pom.xml
-export release_version=2.1-staging
+if [ -z ${release_version} ]; then
+    release_version=2.1-staging
+fi
+export release_version
 
 dir=$(dirname ${0})
 cd ${dir}/../..
 
 echo "KAP Release Version: ${release_version}"
-sh build/script/package.sh
+sh build/script/package.sh $@
 
 if [ -f *.license ]; then
     kap_dir=`tar -tf dist/kylin-kap-*-obf.tar.gz|head -1`
