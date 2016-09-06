b6795c369639f8f6c8922fef2652db0fd2c60eaa
lidongsjtu <lidong@apache.org>
2016-09-06 19:17:01 +0800

    minor, fix properties and diagnosis in package

--- build/script/prepare_kybot.sh
+++ build/script/prepare_kybot.sh
@@ -9,10 +9,10 @@ exportProjectVersions
 echo "Building kybot client..."
 rm -rf build/kybot
 mkdir build/kybot
-cp kybot/core/target/kybot-client-core-*-assembly.jar build/kybot/kybot-client-${release_version}.jar
+cp kybot/core/target/kybot-client-core-*-assembly.jar build/kybot/kybot-client-lib.jar
 cp kybot/build/bin/kybot.sh build/kybot/kybot.sh
 cp kybot/build/kap/diag.sh build/kybot/diag.sh
 
 # Copied file becomes 000 for some env (e.g. Cygwin)
-chmod 644 build/kybot/kybot-client-${release_version}.jar
+chmod 644 build/kybot/kybot-client-lib.jar
 chmod +x build/kybot/kybot.sh
