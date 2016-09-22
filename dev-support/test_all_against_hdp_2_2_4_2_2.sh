a4735041eea4efb60082b3b390509157ffb999df
Hongbin Ma <mahongbin@apache.org>
2016-09-22 10:47:19 +0800

    update kylin

--- dev-support/test_all_against_hdp_2_2_4_2_2.sh
+++ dev-support/test_all_against_hdp_2_2_4_2_2.sh
@@ -32,4 +32,5 @@ mvn clean install -DskipTests                                  2>&1 | tee testal
 mvn test -Dhdp.version=$hdpv -fae                              2>&1 | tee testall-2.log  || exit 1
 mvn -pl :kap-it pre-integration-test -Dhdp.version=$hdpv       2>&1 | tee testall-3.log  || exit 1
 mvn -pl :kap-it failsafe:integration-test -Dhdp.version=$hdpv  2>&1 | tee testall-4.log  || exit 1
+mvn -pl :kap-it post-integration-test -Dhdp.version=$hdpv       2>&1 | tee testall-3.log  || exit 1
 mvn -pl :kap-it failsafe:verify -Dhdp.version=$hdpv            2>&1 | tee testall-5.log  || exit 1
