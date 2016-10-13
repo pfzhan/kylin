3025e2d67db3e975a94b63a618c8efd0508eaac0
Yiming Liu <liuyiming.vip@gmail.com>
2016-10-14 00:17:01 +0800

    #123: Enable License Header Check

--- extensions/core-metadata/src/main/java/io/kyligence/kap/measure/percentile/PercentileMeasureTypeFactory.java
+++ extensions/core-metadata/src/main/java/io/kyligence/kap/measure/percentile/PercentileMeasureTypeFactory.java
@@ -1,3 +1,27 @@
+/**
+ * Copyright (C) 2016 Kyligence Inc. All rights reserved.
+ *
+ * http://kyligence.io
+ *
+ * This software is the confidential and proprietary information of
+ * Kyligence Inc. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * Kyligence Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package io.kyligence.kap.measure.percentile;
 
 import org.apache.kylin.measure.MeasureType;
@@ -5,9 +29,6 @@ import org.apache.kylin.measure.MeasureTypeFactory;
 import org.apache.kylin.metadata.datatype.DataType;
 import org.apache.kylin.metadata.datatype.DataTypeSerializer;
 
-/**
- * Created by dongli on 5/19/16.
- */
 public class PercentileMeasureTypeFactory extends MeasureTypeFactory<PercentileCounter> {
     public static final String FUNC_PERCENTILE = "PERCENTILE";
     public static final String DATATYPE_PERCENTILE = "percentile";
