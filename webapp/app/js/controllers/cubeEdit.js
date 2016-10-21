95cbc16fa2a514bb308c761bc9c6eb8f30752db5
Jason <jiatuer@163.com>
2016-10-21 14:31:35 +0800

    minor, GUI allow view cube json remove 'Disable' limit, add breadcrumb

--- webapp/app/js/controllers/cubeEdit.js
+++ webapp/app/js/controllers/cubeEdit.js
@@ -505,7 +505,6 @@ KylinApp.controller('CubeEditCtrl', function ($scope, $q, $routeParams, $locatio
 
         }
 
-
       }
       else {
         $scope.saveCubeRollBack();
@@ -513,6 +512,38 @@ KylinApp.controller('CubeEditCtrl', function ($scope, $q, $routeParams, $locatio
     });
   };
 
+  //save rawTable
+  function saveRawTable(){
+    if($scope.RawTables&&$scope.RawTables.columns){
+
+      $scope.RawTables.name=$scope.cubeMetaFrame.name;
+      $scope.RawTables.model_name=$scope.cubeMetaFrame.model_name;
+      $scope.RawTables.engine_type=$scope.cubeMetaFrame.engine_type;
+      $scope.RawTables.storage_type=$scope.cubeMetaFrame.storage_type;
+      RawTablesService.save({},{
+        rawTableDescData:angular.toJson($scope.RawTables, true),
+        project: $scope.state.project
+      },function(request){
+
+      },function(request){
+
+      })
+    }
+  }
+
+  //update rawTable
+  function updateRawTable(){
+    RawTablesService.update({},{
+      rawTableDescData:angular.toJson($scope.RawTables, true),
+      project: $scope.state.project,
+      rawTableName:$scope.RawTables.name
+
+    },function(request){
+
+    },function(request){
+
+    })
+  }
 
 //    reverse the date
   $scope.saveCubeRollBack = function () {
