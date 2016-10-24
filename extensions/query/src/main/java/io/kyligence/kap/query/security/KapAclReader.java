31eecdd346886807b0b80dc423d299d30ea48995
Cheng Wang <cheng.wang@kyligence.io>
2016-10-24 12:45:47 +0800

    #71 fix bug

--- extensions/query/src/main/java/io/kyligence/kap/query/security/KapAclReader.java
+++ extensions/query/src/main/java/io/kyligence/kap/query/security/KapAclReader.java
@@ -1,3 +1,27 @@
+/**
+ * Copyright (C) 2016 Kyligence Inc. All rights reserved.
+ *
+ * http://kyligence.io
+ *
+ * This software is the confidential and proprietary information of
+ * Kyligence Inc. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * Kyligence Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package io.kyligence.kap.query.security;
 
 import java.io.BufferedReader;
@@ -10,9 +34,9 @@ import java.util.Map;
 
 import org.apache.kylin.common.KapConfig;
 import org.apache.kylin.common.KylinConfig;
-import org.apache.kylin.metadata.model.ColumnDesc;
 import org.apache.kylin.metadata.model.DataModelDesc;
-import org.apache.kylin.metadata.model.TableDesc;
+import org.apache.kylin.metadata.model.TableRef;
+import org.apache.kylin.metadata.model.TblColRef;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -64,8 +88,8 @@ public class KapAclReader {
         for (String colName : allAclColumns) {
             boolean isColumnsInFact = false;
             boolean isColumnsInLookups = false;
-            for (ColumnDesc colDesc : dmDesc.getFactTableDesc().getColumns()) {
-                String nameInTable = colDesc.getTable().getName() + "." + colDesc.getName();
+            for (TblColRef colDesc : dmDesc.getFactTableRef().getColumns()) {
+                String nameInTable = colDesc.getCanonicalName();
                 if (nameInTable.equalsIgnoreCase(colName)) {
                     isColumnsInFact = true;
                     break;
@@ -75,9 +99,9 @@ public class KapAclReader {
             if (isColumnsInFact)
                 continue;
 
-            for (TableDesc tableDesc : dmDesc.getLookupTableDescs()) {
-                for (ColumnDesc colDesc : tableDesc.getColumns()) {
-                    String nameInTable = colDesc.getTable().getName() + "." + colDesc.getName();
+            for (TableRef tableRef : dmDesc.getLookupTableRefs()) {
+                for (TblColRef tblColRef : tableRef.getColumns()) {
+                    String nameInTable = tblColRef.getCanonicalName();
                     if (nameInTable.equalsIgnoreCase(colName)) {
                         isColumnsInLookups = true;
                         break;
