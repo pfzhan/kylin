cff6da1dfffbbc41c0d71b067f02fe943c0421a8
Yiming Liu <liuyiming.vip@gmail.com>
2016-11-10 17:58:00 +0800

    KYLIN-2133: minor fix

--- build/bin/kylin.sh
+++ build/bin/kylin.sh
@@ -82,7 +82,7 @@ then
     fi
     verbose "kylin.rest.address is set to ${kylin_rest_address}"
 
-    kylin_rest_address_arr=(${kylin_rest_address//;/ })
+    kylin_rest_address_arr=(${kylin_rest_address//:/ })
     nc -z -w 5 ${kylin_rest_address_arr[0]} ${kylin_rest_address_arr[1]} 1>/dev/null 2>&1; nc_result=$?
     if [ $nc_result -eq 0 ]; then
         quit "Port ${kylin_rest_address} is not available, could not start Kylin"
