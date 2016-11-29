726765868be40f3953aa4d7d9dd1eb33eeb4700c
lidongsjtu <lidong@apache.org>
2016-11-29 15:15:35 +0800

    #197 add more check and license file into hotfix build script

--- build/hotfix/build.sh
+++ build/hotfix/build.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-if [[ $# -ne 2 ]]; then
-    echo "Usage: build.sh <tar package path> <base commit id>"
+if [ $# -ne 2 ] && [ $# -ne 3 ]; then
+    echo "Usage: build.sh <tar package path> <base commit id> [<license_file>]"
     exit 1
 fi
 
 package_tar=$1
 base_commit=$2
+license_file=$3
 
 working_dir=temp_hotfix
 mkdir $working_dir
@@ -22,6 +23,12 @@ echo ${kap_dir} > name
 echo ${base_commit}@KAP > base_commit
 cd lib
 
+# copy license file
+if [ ! -z "${license_file}" ]; then
+    rm *.license
+    cp $license_file .
+fi
+
 # remove spark files
 rm -rf spark
 # remove tomcat files
@@ -31,8 +38,14 @@ cd ../../../
 
 # add base conf and bin
 mkdir ${working_dir}/base
-backup_commit=`git rev-parse HEAD`
+current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 git checkout $base_commit
+
+if [ "$?" == "1" ]; then
+    echo "Failed to checkout base commit. Please have a check."
+    exit 1
+fi
+
 cp -r ${dir}/../bin ${working_dir}/base/
 cp -r ${dir}/../conf ${working_dir}/base/
 if [[ ${kap_dir} =~ "plus" ]]; then
@@ -40,7 +53,7 @@ if [[ ${kap_dir} =~ "plus" ]]; then
     cat ${working_dir}/base/conf/plus/kap-plus.prod.properties >> ${working_dir}/base/conf/kap-plus.prod.properties
 fi
 rm -rf ${working_dir}/base/conf/plus
-git checkout $backup_commit
+git checkout $current_branch
 
 mv ${working_dir} ${kap_dir}-hotfix
 mkdir -p ${dir}/../../dist
