8c2df49a23724042ef9dca3d0fe57a4200452a0a
lidongsjtu <lidong@apache.org>
2016-12-08 11:57:51 +0800

    Update license check info

--- extensions/server-base/src/main/java/io/kyligence/kap/rest/Log4jConfigListener.java
+++ extensions/server-base/src/main/java/io/kyligence/kap/rest/Log4jConfigListener.java
@@ -162,8 +162,8 @@ public class Log4jConfigListener extends org.springframework.web.util.Log4jConfi
                 BufferedReader reader = new BufferedReader(new StringReader(lic));
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    if (line.startsWith("kap.parallel.scale:")) {
-                        parallel = Integer.parseInt(line.substring("kap.parallel.scale:".length()).trim());
+                    if (line.startsWith("Parallel Scale:")) {
+                        parallel = Integer.parseInt(line.substring("Parallel Scale:".length()).trim());
                     }
                 }
                 reader.close();
