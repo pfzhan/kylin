40ffb356a6698700671d53ff5e2b675e188cc738
luguosheng <550175214@qq.com>
2016-12-08 17:54:52 +0800

    minor,code rebuild

---
+++ webapp/app/js/utils/dialogHelper.js
@@ -0,0 +1,11 @@
+/**
+ * Created by luguosheng on 16/12/8.
+ */
+'use strict';
+/* Dialog helper*/
+KylinApp.factory('DialogHelper', function ($modal, $timeout, $location, $anchorScroll, $window) {
+  return {
+      alert:function(){},
+      confirm:function(){}
+  }
+});
---
+++ webapp/app/js/utils/stringHelper.js
@@ -0,0 +1,10 @@
+/**
+ * Created by luguosheng on 16/12/8.
+ */
+'use strict';
+/* String helper*/
+KylinApp.factory('StringHelper', function ($modal, $timeout, $location, $anchorScroll, $window) {
+  return {
+
+  }
+});
