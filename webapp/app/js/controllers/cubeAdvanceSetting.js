b43dd4ba6bfaa2309cfe9d3c7dbe142c87646b6e
luguosheng <550175214@qq.com>
2016-12-08 11:40:17 +0800

    #87  right tips cannot link with the left check

--- webapp/app/js/controllers/cubeAdvanceSetting.js
+++ webapp/app/js/controllers/cubeAdvanceSetting.js
@@ -479,7 +479,7 @@ KylinApp.controller('CubeAdvanceSettingCtrl', function ($scope, $modal,cubeConfi
   }
   $scope.getFullTaleNameByColumnName=function(columnName){
     for(var i= 0,len=$scope.cubeMetaFrame.dimensions.length||0;i<len;i++){
-      if($scope.cubeMetaFrame.dimensions[i].name.indexOf(columnName)){
+      if(($scope.cubeMetaFrame.dimensions[i].table+'.'+$scope.cubeMetaFrame.dimensions[i].column).indexOf(columnName)>=0){
          return $scope.cubeMetaFrame.dimensions[i].table;
       }
     }
