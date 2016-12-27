b81c994d142dcba14a2150d85a61ebf56791e2c3
Li Yang <liyang@apache.org>
2016-12-27 13:42:38 +0800

    KYLIN-2314 make default engines configurable

--- extensions/core-common/src/main/java/io/kyligence/kap/common/util/LocalTempMetadata.java
+++ extensions/core-common/src/main/java/io/kyligence/kap/common/util/LocalTempMetadata.java
@@ -29,6 +29,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.kylin.common.util.Pair;
 
 public class LocalTempMetadata {
     public static final String KAP_META_TEST_DATA = "../examples/test_case_data/localmeta";
@@ -41,28 +42,32 @@ public class LocalTempMetadata {
 
             // KAP files will overwrite Kylin files
             FileUtils.copyDirectory(new File(KYLIN_META_TEST_DATA), new File(TEMP_TEST_METADATA));
-            
-            overrideEngineTypeAndStorageType(TEMP_TEST_METADATA);
-            
             FileUtils.copyDirectory(new File(KAP_META_TEST_DATA), new File(TEMP_TEST_METADATA));
-            
+
+            appendKylinProperties(TEMP_TEST_METADATA);
+            overrideEngineTypeAndStorageType(TEMP_TEST_METADATA);
+
             return TEMP_TEST_METADATA;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void overrideEngineTypeAndStorageType(String tempMetadataDir) throws IOException {
+    private static void appendKylinProperties(String tempMetadataDir) throws IOException {
         // append kylin.properties
-        String kylinPropsAppend = "\n" //
-                + "kylin.storage.provider.100=io.kyligence.kap.storage.parquet.ParquetStorage" + "\n" //
-                + "kylin.engine.provider.100=io.kyligence.kap.engine.mr.KapMRBatchCubingEngine" + "\n" //
-                + "kylin.engine.default=100" + "\n" //
-                + "kylin.storage.default=100" + "\n" //
-                + "kylin.metadata.realization-providers=org.apache.kylin.cube.CubeManager,org.apache.kylin.storage.hybrid.HybridManager,io.kyligence.kap.cube.raw.RawTableManager" + "\n"
-                + "kylin.metadata.custom-measure-types.percentile=io.kyligence.kap.measure.percentile.PercentileMeasureTypeFactory" + "\n";
-        FileUtils.writeStringToFile(new File(tempMetadataDir, "kylin.properties"), kylinPropsAppend, "UTF-8", true);
-        
+        File appendFile = new File(tempMetadataDir, "kylin.properties.append");
+        if (appendFile.exists()) {
+            String appendStr = FileUtils.readFileToString(appendFile, "UTF-8");
+            FileUtils.writeStringToFile(new File(tempMetadataDir, "kylin.properties"), appendStr, "UTF-8", true);
+            appendFile.delete();
+        }
+    }
+
+    private static void overrideEngineTypeAndStorageType(String tempMetadataDir) throws IOException {
+        Pair<Integer, Integer> pair = grabDefaultEngineTypes(tempMetadataDir);
+        int engineType = pair.getFirst();
+        int storageType = pair.getSecond();
+
         // re-write cube_desc/*.json
         File cubeDescDir = new File(tempMetadataDir, "cube_desc");
         for (File f : cubeDescDir.listFiles()) {
@@ -70,13 +75,42 @@ public class LocalTempMetadata {
             for (int i = 0, n = lines.size(); i < n; i++) {
                 String l = lines.get(i);
                 if (l.contains("\"engine_type\"")) {
-                    lines.set(i, "  \"engine_type\" : 100,");
+                    lines.set(i, "  \"engine_type\" : " + engineType + ",");
                 }
                 if (l.contains("\"storage_type\"")) {
-                    lines.set(i, "  \"storage_type\" : 100,");
+                    lines.set(i, "  \"storage_type\" : " + storageType + ",");
                 }
             }
             FileUtils.writeLines(f, "UTF-8", lines);
         }
+    }
+
+    private static Pair<Integer, Integer> grabDefaultEngineTypes(String tempMetadataDir) throws IOException {
+        int engineType = -1;
+        int storageType = -1;
+        
+        List<String> lines = FileUtils.readLines(new File(tempMetadataDir, "kylin.properties"), "UTF-8");
+        for (String l : lines) {
+            if (l.startsWith("kylin.engine.default")) {
+                engineType = Integer.parseInt(l.substring(l.lastIndexOf('=') + 1).trim());
+            }
+            if (l.startsWith("kylin.storage.default")) {
+                storageType = Integer.parseInt(l.substring(l.lastIndexOf('=') + 1).trim());
+            }
+        }
+        
+        String tmp = System.getProperty("kylin.engine");
+        if (tmp != null) {
+            engineType = Integer.parseInt(tmp.trim());
+        }
+        tmp = System.getProperty("kylin.storage");
+        if (tmp != null) {
+            storageType = Integer.parseInt(tmp.trim());
+        }
+        
+        if (engineType < 0 || storageType < 0)
+            throw new IllegalStateException();
+        
+        return Pair.newPair(engineType, storageType);
     }
 }
