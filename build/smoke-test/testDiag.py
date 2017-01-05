312672c787991be2ecc545e208debf2cdb8f7fda
lidongsjtu <lidong@apache.org>
2017-01-05 09:04:08 +0800

    minor, print response text in smoke test

--- build/smoke-test/testDiag.py
+++ build/smoke-test/testDiag.py
@@ -19,8 +19,9 @@ class testDiag(unittest.TestCase):
         }
 
         response = requests.get(url, headers = headers)
-        self.assertEqual(response.status_code, 200, 'Diagnosis failed.')
 
+        self.assertEqual(response.status_code, 200, 'Diagnosis failed.')
+        print response.text
 
 if __name__ == '__main__':
     print 'Test Diagnogis for Kylin sample.'
