064337d682785e5154a51fefe657d1c380721227
lidongsjtu <lidong@apache.org>
2017-02-10 16:45:51 +0800

    #315 refine scripts

--- build/smoke-test/testLib.sh
+++ build/smoke-test/testLib.sh
@@ -8,17 +8,16 @@ for prefix in $HADOOP_PREFIX; do
     for dir in $CHK_DIRS; do
         echo "Check if we shipped class with prefix \"$prefix\" in \"$dir\"".
         if [ ! -d "$dir" ]; then
-            echo "Dir not exists"
-            continue
-        fi
-
-        grep -R $KYLIN_HOME/$prefix $dir
-        if [ "$?" = "0" ]; then
-            echo "========================================"
-            echo "We should not ship classes/jars of hadoop/hbase/hive, but found one."
-            echo "Please check the console output above."
-            echo "========================================"
-            exit 1
+            echo "Dir $dir not exists"
+        else
+            grep -R $KYLIN_HOME/$prefix $dir
+            if [ "$?" = "0" ]; then
+                echo "========================================"
+                echo "We should not ship classes/jars of hadoop/hbase/hive, but found one."
+                echo "Please check the console output above."
+                echo "========================================"
+                exit 1
+            fi
         fi
     done
 done
