3fa1a0bdee750d07b3ca461d4ea5a664aeb0786a
Yifan Zhang <event.dimlas@gmail.com>
2017-02-16 15:56:44 +0800

    Remove submodule "kybot(kybot-client)"
    Use github released kybot client

---
+++ build/kybot_kap/diag.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Kyligence Inc. License
+
+# We should set KYLIN_HOME here for multiple tomcat instances that are on the same node.
+# In addition, we should set a KYLIN_HOME for the global use as normal.
+KYLIN_HOME=`dirname $0`/..
+export KYLIN_HOME=`cd "$KYLIN_HOME"; pwd`
+dir="$KYLIN_HOME/bin"
+
+mkdir -p ${KYLIN_HOME}/logs
+
+tomcat_root=${dir}/../tomcat
+export tomcat_root
+
+if [ $# -eq 1 ] || [ $# -eq 2 ] || [ $# -eq 3 ]
+then
+    patient="$1"
+    if [ -z "$patient" ]
+    then
+        echo "You need to specify a Project or Job Id for diagnosis."
+        exit 1
+    fi
+    destDir="$2"
+    if [ -z "$destDir" ]
+    then
+        destDir="$KYLIN_HOME/diagnosis_dump/"
+        mkdir -p $destDir
+    fi
+    needUpload="$3"
+
+    if [ ${#patient} -eq 36 ]; then
+        source ${dir}/find-hive-dependency.sh
+
+        #retrive $KYLIN_EXTRA_START_OPTS
+        if [ -f "${dir}/setenv-tool.sh" ]
+            then source ${dir}/setenv-tool.sh
+        fi
+
+        mkdir -p ${KYLIN_HOME}/ext
+        export HBASE_CLASSPATH_PREFIX=${KYLIN_HOME}/conf:${KYLIN_HOME}/lib/*:${KYLIN_HOME}/tool/*:${KYLIN_HOME}/ext/*:${HBASE_CLASSPATH_PREFIX}
+        export HBASE_CLASSPATH=${HBASE_CLASSPATH}:${hive_dependency}
+
+        hbase ${KYLIN_EXTRA_START_OPTS} \
+        -Dlog4j.configuration=kylin-server-log4j.properties \
+        -Dcatalina.home=${tomcat_root} \
+        org.apache.kylin.tool.JobDiagnosisInfoCLI \
+        -jobId $patient \
+        -destDir $destDir || exit 1
+    else
+        KYBOT_OPTS=""
+        if [ $needUpload = "true" ]; then
+            KYBOT_OPTS="-uploadToServer true"
+        fi
+        # will use kybot as system diagnosis
+        sh ${KYLIN_HOME}/kybot/kybot.sh $KYBOT_OPTS -destDir $destDir
+    fi
+
+    exit 0
+else
+    echo "usage: diag.sh Project|JobId [target_path]"
+    exit 1
+fi
--- build/script/prepare_kybot.sh
+++ build/script/prepare_kybot.sh
@@ -6,15 +6,19 @@ cd ${dir}/../..
 source build/script/functions.sh
 exportProjectVersions
 
-echo "Building kybot client..."
+echo "Fetching kybot client..."
+curl -u Kyligence:511e7eaf39667ef98826be54e7ff0447c9494cf1 -H "Accept: application/octet-stream" -L https://api.github.com/repos/Kyligence/kybot-client/releases/assets/3237215 > kybot-client.tar.gz
+tar zxf kybot-client.tar.gz
+
+echo "Preparing kybot client..."
 rm -rf build/kybot
-mkdir build/kybot
-cp kybot/core-161/target/kybot-client-core-*-assembly.jar build/kybot/kybot-client-lib.jar
-cp kybot/build/bin/kybot.sh build/kybot/kybot.sh
-cp kybot/build/conf/kybot-client.properties build/kybot/kybot-client.properties
-cp kybot/build/kap/diag.sh build/kybot/diag.sh
+cp -r kybot-client-*-bin/kybot build/kybot
+cp build/kybot_kap/diag.sh build/kybot/diag.sh
 cp build/bin/header.sh build/kybot/header.sh
+rm -rf kybot-client-*-bin
 
 # Copied file becomes 000 for some env (e.g. Cygwin)
 chmod 644 build/kybot/kybot-client-lib.jar
 chmod +x build/kybot/kybot.sh
+
+echo "Kybot client is ready."
