5177b6ddaa63c2b3530bdb307df96e0d67550efb
xiefan46 <958034172@qq.com>
2017-02-17 18:18:05 +0800

    #409 Create setup script. Auto config some spark properties

---
+++ build/bin/setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#three args $key $value $filename
+function add_or_reset()
+{
+        key=$1
+        value=$2
+        filename=$3
+        line=`sed -n '/'"$key"'/=' $filename`
+        if [ "$line" == "" ]; then  #Not exist. Append
+                echo "${key}=${value}" >> ${filename}
+                return
+        else #Exist. Replace
+                sed -i '/'"$key"'/d' ${filename}
+                echo "${key}=${value}" >> ${filename}
+        fi
+}
+
+#def constant var
+spark_executor_cores=4
+override_file="../conf/kylin.properties"
+
+if [ ! -f ${override_file} ]; then
+        echo "${override_file} not exist. Please check"
+        exit 1
+fi
+
+
+echo "start setup procedure"
+echo "please enter the number of spark vcore. You'd better make sure that it's multiple of ${spark_executor_cores}"
+read spark_vcore
+spark_executor_instances=`expr ${spark_vcore} / ${spark_executor_cores} `
+
+echo "auto config properties below"
+echo "kap.storage.columnar.spark-conf.spark.executor.instances=${spark_executor_instances}"
+echo "kap.storage.columnar.spark-conf.spark.executor.cores=${spark_executor_cores}"
+
+
+add_or_reset kap.storage.columnar.spark-conf.spark.executor.instances ${spark_executor_instances} ${override_file}
+add_or_reset kap.storage.columnar.spark-conf.spark.executor.cores ${spark_executor_cores} ${override_file}
+
+
+
