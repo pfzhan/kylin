e74247761aeddf1cb7bd6479211f01b8f7ef0a16
Billy Liu <billyliu@apache.org>
2017-02-20 22:29:46 +0800

    KYLIN-2428 fix Storage Parquet package

--- build/bin/get-properties.sh
+++ build/bin/get-properties.sh
@@ -29,10 +29,8 @@ fi
 
 if [[ $CI_MODE == 'true' ]]; then
     cd $dir
-    job_jar=$(ls ../../kylin/assembly/target/kylin-*-job.jar)
-    tool_jar=$(ls ../../kylin/tool/target/kylin-tool-*.jar|grep -v assembly|grep -v tests)
+    tool_jar=$(ls ../../kylin/tool-assembly/target/kylin-tool-*-assembly.jar)
 else
-    job_jar=$(ls $KYLIN_HOME/lib/kylin-job-*.jar)
     tool_jar=$(ls $KYLIN_HOME/lib/kylin-tool-*.jar)
 fi
 
@@ -40,5 +38,5 @@ kylin_conf_opts=
 if [ ! -z "$KYLIN_CONF" ]; then
     kylin_conf_opts="-DKYLIN_CONF=$KYLIN_CONF"
 fi
-result=`java $kylin_conf_opts -cp $job_jar:$tool_jar org.apache.kylin.tool.KylinConfigCLI $1 2>/dev/null`
+result=`java $kylin_conf_opts -cp $tool_jar -Dlog4j.configuration=file:${KYLIN_HOME}/conf/kylin-tools-log4j.properties org.apache.kylin.tool.KylinConfigCLI $1 2>/dev/null`
 echo "$result"
