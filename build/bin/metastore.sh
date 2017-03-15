f088d7ba74141f7113dddd04433bb26fee1899fe
Hongbin Ma <mahongbin@apache.org>
2017-03-15 13:53:39 +0800

    refine metastore.sh refresh-cube-signature

--- build/bin/metastore.sh
+++ build/bin/metastore.sh
@@ -32,6 +32,7 @@ function help {
     echo "usage: metastore.sh backup"
     echo "       metastore.sh fetch DATA"
     echo "       metastore.sh reset"
+    echo "       metastore.sh refresh-cube-signature"
     echo "       metastore.sh restore PATH_TO_LOCAL_META"
     echo "       metastore.sh list RESOURCE_PATH"
     echo "       metastore.sh cat RESOURCE_PATH"
