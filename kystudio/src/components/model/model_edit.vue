61708e793d710b8fc129041862300b4ba83445d5
luguosheng <550175214@qq.com>
2017-03-23 18:57:52 +0800

    #488 drag part code

--- kystudio/src/components/model/model_edit.vue
+++ kystudio/src/components/model/model_edit.vue
@@ -3,10 +3,11 @@
       <div class="table_box" v-for="table in tableList" :id="table.guid" >
         <span class="tool_box"><i class="el-icon-setting"></i></span>
         <p class="table_name">{{table.alias||table.name}}</p>
-        <p class="filter_box"><el-input v-model="table.filterName" v-on:change="filterColumn(table.filterName,table.guid)"  size="small" placeholder="enter filter..."></el-input></p>
+        <div class="link_box" v-if="selectColumn[table.guid]"><i class="el-icon-close" v-on:click="cancelFilterColumn(table.guid)"></i>{{selectColumn[table.guid]&&selectColumn[table.guid].columnName}}</div>
+        <p class="filter_box"><el-input v-model="table.filterName" v-on:change="filterColumnByInput(table.filterName,table.guid)"  size="small" placeholder="enter filter..."></el-input></p>
         <section data-scrollbar class="columns_box">
           <ul>
-            <li v-for="column in table.columns" :key="column.guid" :class="column.name" class="column_li"><span class="kind">D</span><span class="column">{{column.name}}</span><span class="column_type">{{column.datatype}}</span></style></li>
+            <li v-on:click="selectFilterColumn(table.guid,column.name,column.datatype)" v-for="column in table.columns" :key="column.guid" :class="column.name" class="column_li"><span class="kind">D</span><span class="column">{{column.name}}</span><span class="column_type">{{column.datatype}}</span></style></li>
           </ul>
         </section>
         <div class="more_tool"><i class="el-icon-d-caret"></i></div>
@@ -21,7 +22,10 @@ import Scrollbar from 'smooth-scrollbar'
 export default {
   data () {
     return {
+      selectColumn: {},
       plumbInstance: null,
+      plumbInstanceForShowLink: null,
+      links: [],
       tableList: [{
         'filterName': '',
         'name': 'test1',
@@ -34,26 +38,107 @@ export default {
         'database': 'default',
         'alias': 'Product',
         'guid': sampleGuid(),
-        'columns': [{'id': '1', 'name': 'LEAF_CATEG_ID1', 'datatype': 'bigint'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '1', 'name': 'LEAF_CATEG_ID', 'datatype': 'bigint'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}]
-      }]
+        'columns': [{'id': '1', 'name': 'LEAF_CATEG_ID1', 'datatype': 'bigint'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '1', 'name': 'LEAF_CATEG_ID', 'datatype': 'bigint'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '1', 'name': 'LEAF_CATEG_ID1', 'datatype': 'bigint'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '1', 'name': 'LEAF_CATEG_ID', 'datatype': 'bigint'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}]
+      }, {
+        'name': 'test22',
+        'database': 'default',
+        'alias': 'Product',
+        'guid': sampleGuid(),
+        'columns': [{'id': '1', 'name': 'LEAF_CATEG_ID1', 'datatype': 'bigint'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '1', 'name': 'LEAF_CATEG_ID', 'datatype': 'bigint'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '1', 'name': 'LEAF_CATEG_ID1', 'datatype': 'bigint'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '1', 'name': 'LEAF_CATEG_ID', 'datatype': 'bigint'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}, {'id': '2', 'name': 'LEAF_CATEG_NAME', 'datatype': 'varchar(256)'}]
+      }],
+      endpointConfig: function (type) {
+        var connectorPaintStyle = {
+          strokeWidth: 2,
+          stroke: '#61B7CF',
+          joinstyle: 'round',
+          outlineStroke: 'white',
+          outlineWidth: 2
+        }
+    // .. and this is the hover style.
+        var connectorHoverStyle = {
+          strokeWidth: 3,
+          stroke: '#216477',
+          outlineWidth: 5,
+          outlineStroke: 'white'
+        }
+        var endpointHoverStyle = {
+          fill: '#216477',
+          stroke: '#216477'
+        }
+        var sourceEndpoint = {
+          endpoint: 'Dot',
+          paintStyle: {
+            stroke: '#7AB02C',
+            fill: 'transparent',
+            radius: 7,
+            strokeWidth: 1
+          },
+          isSource: true,
+          connector: [ 'Flowchart', { stub: [40, 60], gap: 10, cornerRadius: 5, alwaysRespectStubs: true } ],
+          connectorStyle: connectorPaintStyle,
+          hoverPaintStyle: endpointHoverStyle,
+          connectorHoverStyle: connectorHoverStyle,
+          dragOptions: {},
+          overlays: [
+            [ 'Label', {
+              location: [0.5, 1.5],
+              label: 'Drag',
+              cssClass: 'endpointSourceLabel',
+              visible: false
+            } ]
+          ]
+        }
+    // the definition of target endpoints (will appear when the user drags a connection)
+        var targetEndpoint = {
+          endpoint: 'Dot',
+          paintStyle: { fill: '#7AB02C', radius: 7 },
+          hoverPaintStyle: endpointHoverStyle,
+          maxConnections: -1,
+          dropOptions: { hoverClass: 'hover', activeClass: 'active' },
+          isTarget: true,
+          overlays: [
+                [ 'Label', { location: [0.5, -0.5], label: 'Drop', cssClass: 'endpointTargetLabel', visible: false } ]
+          ]
+        }
+        if (type === 'source') {
+          return sourceEndpoint
+        } else {
+          return targetEndpoint
+        }
+      }
     }
   },
   beforeDestroy () {
-    this.plumbInstance.deleteEveryEndpoint()
+    this.removeAllEndpoints(this.plumbInstance)
   },
   methods: {
-    filterColumn: function (filter, id) {
-      // console.log(filter, id)
+    /*
+    *  Filter Func
+    *  ==========================================================================
+    */
+    filterColumnByInput: function (filter, id) {
       var instance = Scrollbar.get($('#' + id).find('.columns_box')[0])
-      console.log(instance, document.getElementById(id))
-      var suggestObj = this.getColumnByLikeFilter(id, filter)
-      // console.log(this.getColumnByLikeFilter(id, filter))
-      // document.querySelector('.' + this.getColumnByLikeFilter(id, filter)).scrollIntoView()
+      var suggestObj = this.getColumnDataByLikeFilter(id, filter)
       this.autoScroll(instance, suggestObj.index * 30, suggestObj.className, id)
+      this.selectFilterColumn(id, suggestObj.className, suggestObj.columnType, 'target')
     },
-    getColumnByLikeFilter: function (guid, filter) {
+    cancelFilterColumn: function (id) {
+      this.$set(this.selectColumn, id, '')
+      // this.removeAllEndpoints(this.plumbInstance)
+    },
+    selectFilterColumn: function (id, columnName, columnType, pointType) {
+      pointType = pointType || 'source'
+      if (columnName) {
+        this.$set(this.selectColumn, id, {columnName: columnName, columnType: columnType})
+        this.addSelectPoints(id, this.plumbInstance, pointType, columnName, columnType)
+      } else {
+        this.cancelFilterColumn(id)
+      }
+    },
+    getColumnDataByLikeFilter: function (guid, filter) {
       var suggest = {
         className: '',
+        columnType: '',
         index: 0
       }
       if (filter === '') {
@@ -61,92 +146,175 @@ export default {
       }
       this.tableList.forEach(function (table) {
         if (table.guid === guid) {
-          table.columns.forEach(function (col, index) {
+          for (let i = 0; i < table.columns.length; i++) {
+            var col = table.columns[i]
             if (col.name.toUpperCase().indexOf(filter.toUpperCase()) >= 0) {
               suggest.className = col.name
-              suggest.index = index
-              return suggest
+              suggest.index = i
+              suggest.columnType = col.datatype
+              break
             }
-          })
+          }
         }
       })
       return suggest
     },
-    draggleTable: function (jsPlumb, idList) {
-      jsPlumb.draggable(idList)
-    },
     autoScroll (instance, topSize, aim, id) {
       instance.scrollTo(100, topSize, 300, function (scrollbar) {
-        $('#' + id).find('.' + aim).css('color', 'red')
-        $('#' + id)
-      })
-    },
-    createPointConfig: function (newLinkConfig) {
-      let linkConfig = {
-        // 设置连接点的形状为圆形
-        endpoint: 'Rectangle',
-        // 设置连接点的颜色
-        paintStyle: { fill: '#46b8da', width: 10, height: 10 },
-        // 是否可以拖动（作为连线起点）
-        isSource: true,
-        // 连接点的标识符，只有标识符相同的连接点才能连接
-        scope: 'green dot',
-        // 设置连线为贝塞尔曲线
-        connector: [ 'Bezier', { curviness: 163 } ],
-        // 设置连接点最多可以连接几条线
-        maxConnections: 100,
-        // 是否可以放置（作为连线终点）
-        isTarget: true,
-        connectorStyle: {
-          strokeWidth: 5,
-          stroke: '#66a8fa'
-        },
-        dropOptions: {
-        // 释放时指定鼠标停留在该元素上使用的css class
-          hoverClass: 'dropHover',
-        // 设置放置相关的css
-          activeClass: 'dragActive'
-        },
-        beforeDetach: function (conn) { // 绑定一个函数，在连线前弹出确认框
-         // delete that.connects[info.connection.id];
-        },
-        reattachConnections: function () {
-        // alert(1);
-        },
-        onMaxConnections: function (info) { // 绑定一个函数，当到达最大连接个数时弹出提示框
-        // alert("Cannot drop connection " + info.connection.id + " : maxConnections has been reached on Endpoint " + info.endpoint.id);
-        },
-        onConnectionDetached: function () {
-        // alert(2);
-        },
-        beforeDrop: function () {
-        // that.beginDrag=true;
-          return true
+        $('#' + id).find('.column_li').removeClass('active_filter')
+        if (aim !== '') {
+          $('#' + id).find('.' + aim).addClass('active_filter')
+          instance.addListener(function (status) {
+            // console.log(status)
+          })
         }
-      }
-      linkConfig = Object.assign({}, linkConfig, newLinkConfig)
-      return linkConfig
-    },
-    addPoints: function (jsPlumb) {
-      let _this = this
-      this.tableList.forEach(function (table) {
-        table.columns.forEach(function (columns, i) {
-          var h = (106 / 400 + i * 30 / 400) > 1 ? 1 : (106 / 400 + i * 30 / 400)
-          jsPlumb.addEndpoint(table.guid, {anchor: [[1.0, h, 1.5, 0], [0, h, -1, 0]]}, _this.createPointConfig())
-        })
       })
+      // instance.stop()
+    },
+    linkFilterColumnAnimate (sourceId, targetId, callback) {
+      $('#' + sourceId).find('.link_box').animate({
+        'top': '0',
+        'width': '0px',
+        'height': '0',
+        'left': '125px'
+      })
+      $('#' + targetId).find('.link_box').animate({
+        'top': '0',
+        'width': '0px',
+        'height': '0',
+        'left': '125px'
+      }, 'fast', function () {
+        callback()
+      })
+    },
+    /*
+    *  Table Func
+    *  ==========================================================================
+    */
+    draggleTable: function (idList) {
+      this.plumbInstance.draggable(idList)
+    },
+    /*
+    *  Endpoint Func
+    *  ==========================================================================
+    */
+    createEndpointConfig: function (newEndpointconfig, type) {
+      return Object.assign({}, this.endpointConfig(type), newEndpointconfig)
+    },
+    removeAllEndpoints (plumb) {
+      plumb.deleteEveryEndpoint()
+    },
+    addSelectPoints: function (guid, jsplumb, pointType, columnName, columnType, topPoint) {
+      var anchor = [[1.0, 0.4, 1.5, 0], [0, 0.4, -1, 0]]
+      var scope = 'link'
+      if (topPoint) {
+        anchor = [[0.5, 0, 0.6, 0]]
+        scope = 'showlink'
+      }
+      jsplumb.addEndpoint(guid, {anchor: anchor}, this.createEndpointConfig({
+        scope: scope,
+        parameters: {
+          data: {
+            guid: guid,
+            column: {
+              columnName: columnName,
+              columnType: columnType
+            }}
+        },
+        uuid: guid + columnName
+      }, pointType))
+      // this.draggleTable([guid])
+      this.refreshPlumbObj(jsplumb)
+    },
+    /*
+    *  Connect Func
+    *  ==========================================================================
+    */
+    connect: function (p1, p2, jsplumb, otherProper) {
+      var defaultPata = {uuids: [p1, p2], editable: true}
+      $.extend(defaultPata, otherProper)
+      jsplumb.connect(defaultPata)
+    },
+    addShowLink: function (p1, p2, count) {
+      this.connect(p1, p2, this.plumbInstance)
+      // conn.getOverlay('label').setLabel(count)
+    },
+    refreshPlumbObj: function (plumb) {
+      plumb = plumb || jsPlumb
+      plumb.repaintEverything()
     }
   },
   mounted () {
     let _this = this
     jsPlumb.ready(() => {
-      _this.plumbInstance = jsPlumb.getInstance()
-      _this.draggleTable(_this.plumbInstance, _this.tableIdList)
-      _this.addPoints(_this.plumbInstance)
+      // _this.plumbInstance = jsPlumb.getInstance()
+
+      _this.plumbInstance = jsPlumb.getInstance({
+        DragOptions: { cursor: 'pointer', zIndex: 2000 },
+        ConnectionOverlays: [
+          [ 'Arrow', {
+            location: 1,
+            visible: true,
+            width: 11,
+            length: 11,
+            id: 'ARROW',
+            events: {
+              click: function () { alert('you clicked on the arrow overlay') }
+            }
+          } ],
+          [ 'Label', {
+            location: 0.1,
+            id: 'label',
+            cssClass: 'aLabel',
+            events: {
+              tap: function () { alert('hey') }
+            }
+          }]
+        ],
+        Container: 'canvas'
+      })
+      var basicType = {
+        connector: 'StateMachine',
+        paintStyle: { stroke: 'red', strokeWidth: 4 },
+        hoverPaintStyle: { stroke: 'blue' },
+        overlays: [
+          'Arrow'
+        ]
+      }
+      _this.plumbInstance.registerConnectionType('basic', basicType)
+      _this.draggleTable(_this.tableIdList)
+      _this.plumbInstance.bind('connection', function (info, originalEvent) {
+        console.log(info.sourceEndpoint.getUuid())
+        if (info.connection.scope !== 'showlink') {
+          _this.links[info.connection.id] = [info.sourceEndpoint.getParameters().data, info.targetEndpoint.getParameters().data]
+          // _this.removeAllEndpoints(_this.plumbInstance)
+          _this.plumbInstance.deleteEndpoint(info.sourceEndpoint.getUuid())
+          _this.plumbInstance.deleteEndpoint(info.targetEndpoint.getUuid())
+
+          _this.addSelectPoints(info.connection.sourceId, _this.plumbInstance, 'source', '', '', true)
+          _this.addSelectPoints(info.connection.targetId, _this.plumbInstance, 'target', '', '', true)
+
+          _this.linkFilterColumnAnimate(info.connection.sourceId, info.connection.targetId, function () {
+            _this.cancelFilterColumn(info.connection.sourceId)
+            _this.cancelFilterColumn(info.connection.targetId)
+            _this.addShowLink(info.connection.sourceId, info.connection.targetId, _this.connectsCount)
+          })
+        }
+      })
     })
-    // var el = document.querySelector('.columns_box')
+    _this.plumbInstance.bind('connectionDrag', function (connection) {
+      console.log('connection ' + connection.id + ' is being dragged. suspendedElement is ', connection.suspendedElement, ' of type ', connection.suspendedElementType)
+    })
+
+    _this.plumbInstance.bind('connectionDragStop', function (connection) {
+      console.log('connection ' + connection.id + ' was dragged')
+    })
+
+    _this.plumbInstance.bind('connectionMoved', function (params) {
+      console.log('connection ' + params.connection.id + ' was moved')
+    })
+    jsPlumb.fire('jsPlumbDemoLoaded', this.plumbInstance)
     Scrollbar.initAll()
-    console.log($)
   },
   computed: {
     tableIdList: function () {
@@ -155,6 +323,13 @@ export default {
         tableIdListArr.push(table.guid)
       })
       return tableIdListArr
+    },
+    connectsCount: function () {
+      var count = 0
+      this.links.forEach(function () {
+        count = count + 1
+      })
+      return count
     }
   }
 }
@@ -168,6 +343,21 @@ export default {
        background-color: #64748a; 
        position: absolute;
        height: 422px;
+       .link_box{
+         i{
+           padding: 5px;
+           cursor: pointer;
+         }
+         position: absolute;
+         background-color: #58b7ff;
+         height: 40px;
+         width: 100%;
+         top:150px;
+         color:#fff;
+         line-height: 40px;
+         // text-align: center;
+         z-index: 11;
+       }
        .tool_box{
         position: absolute;
         right: 6px;
@@ -194,7 +384,7 @@ export default {
        }
        .columns_box{
         height: 300px;
-        overflow-y: auto;
+        overflow: hidden;
        }
        ul{
         // position: absolute;
@@ -203,9 +393,13 @@ export default {
           list-style: none;
           font-size: 12px;
           height: 30px;
+          line-height: 30px;
           color:#fff;
           span{
             display: inline-block;
+          }
+          &.active_filter{
+            // color:red;
           }
           .kind{
             color: #20a0ff;
