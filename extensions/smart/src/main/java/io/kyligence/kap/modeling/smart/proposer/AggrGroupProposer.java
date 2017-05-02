b25f9d9e9c0b5d3d0cf611c0d75a6f31f0555dad
Hongbin Ma <mahongbin@apache.org>
2017-05-02 16:36:56 +0800

    fix compile

--- extensions/smart/src/main/java/io/kyligence/kap/modeling/smart/proposer/AggrGroupProposer.java
+++ extensions/smart/src/main/java/io/kyligence/kap/modeling/smart/proposer/AggrGroupProposer.java
@@ -30,7 +30,6 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.SortedSet;
 
 import org.apache.kylin.cube.model.AggregationGroup;
 import org.apache.kylin.cube.model.CubeDesc;
