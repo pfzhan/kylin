26e4ddea08efdcb0f172634863b4746a8c4c4515
auphyroc99 <454530524@qq.com>
2017-05-24 09:48:11 +0800

    Replaced Tab in metastore.sh with Spaces

--- build/bin/metastore.sh
+++ build/bin/metastore.sh
@@ -157,8 +157,8 @@ then
     cp -r $3 $TMP_NAME
     zip -r $TMP_NAME.zip $TMP_NAME
     ${KYLIN_HOME}/bin/kylin.sh org.apache.kylin.tool.CubeMetaIngester -srcPath $TMP_NAME.zip -project $2 -overwriteTables true -forceIngest true
-	rm -r $TMP_NAME
-	rm $TMP_NAME.zip
+    rm -r $TMP_NAME
+    rm $TMP_NAME.zip
 	
 elif [ "$1" == "promote" ]
 then
