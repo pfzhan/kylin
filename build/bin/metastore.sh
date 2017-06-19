37a2f039ab0764428bf39e7abfd6152897cb574a
auphyroc99 <454530524@qq.com>
2017-06-19 14:03:36 +0800

    #754 Refine restore-project like restore-cube

--- build/bin/metastore.sh
+++ build/bin/metastore.sh
@@ -126,8 +126,12 @@ then
         help
     fi
 
-    zip -r $3.zip $3
-    ${KYLIN_HOME}/bin/kylin.sh org.apache.kylin.tool.CubeMetaIngester -srcPath $3.zip -project $2 -overwriteTables true -forceIngest true
+    TMP_NAME=${RANDOM}"-"${RANDOM}"-"${RANDOM}"-"${RANDOM}"-"${RANDOM}
+    cp -rf $3 $TMP_NAME
+    zip -r $TMP_NAME.zip $TMP_NAME
+    ${KYLIN_HOME}/bin/kylin.sh org.apache.kylin.tool.CubeMetaIngester -srcPath $TMP_NAME.zip -project $2 -overwriteTables true -forceIngest true
+    rm -rf $TMP_NAME
+    rm -f $TMP_NAME.zip
 
 elif [ "$1" == "restore-cube" ]
 then
@@ -136,11 +140,11 @@ then
     fi
 
     TMP_NAME=${RANDOM}"-"${RANDOM}"-"${RANDOM}"-"${RANDOM}"-"${RANDOM}
-    cp -r $3 $TMP_NAME
+    cp -rf $3 $TMP_NAME
     zip -r $TMP_NAME.zip $TMP_NAME
     ${KYLIN_HOME}/bin/kylin.sh org.apache.kylin.tool.CubeMetaIngester -srcPath $TMP_NAME.zip -project $2 -overwriteTables true -forceIngest true
-    rm -r $TMP_NAME
-    rm $TMP_NAME.zip
+    rm -rf $TMP_NAME
+    rm -f $TMP_NAME.zip
 	
 elif [ "$1" == "promote" ]
 then
