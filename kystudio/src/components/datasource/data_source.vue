3a70541f4d134bc86d43f17033904bc4f8d79d5c
luguosheng1314 <550175214@qq.com>
2017-08-22 14:50:19 +0800

    #2033

--- kystudio/src/components/datasource/data_source.vue
+++ kystudio/src/components/datasource/data_source.vue
@@ -532,7 +532,7 @@ export default {
     checkTableHasJob (tableName, cb, hasNotCb) {
       this.getTableJob({tableName: tableName, project: this.project}).then((res) => {
         handleSuccess(res, (data) => {
-          if (data && (data.job_status === 'FINISHED' || data.progress === '100') || !data) {
+          if (data && (data.job_status === 'FINISHED' || data.progress === '100' || data.job_status === 'DISCARD') || !data) {
             hasNotCb()
           } else {
             cb()
