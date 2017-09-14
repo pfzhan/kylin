0a97fe038fe812779b83fa722c60cc5caef2ca88
lidongsjtu <lidong@apache.org>
2017-09-14 21:50:07 +0800

    minor, say goodbye to manual configure in DebugTomcat

--- extensions/server/src/main/java/io/kyligence/kap/rest/KAPDebugTomcat.java
+++ extensions/server/src/main/java/io/kyligence/kap/rest/KAPDebugTomcat.java
@@ -33,6 +33,7 @@ import org.apache.catalina.core.AprLifecycleListener;
 import org.apache.catalina.core.StandardServer;
 import org.apache.catalina.deploy.ErrorPage;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.util.Shell;
 import org.apache.kylin.common.KylinConfig;
@@ -55,7 +56,7 @@ public class KAPDebugTomcat {
                 System.setProperty("catalina.home", ".");
 
             if (StringUtils.isEmpty(System.getProperty("hdp.version"))) {
-                throw new RuntimeException("No hdp.version set; Please set hdp.version in your jvm option, for example: -Dhdp.version=2.4.0.0-169");
+                System.setProperty("hdp.version", "2.4.0.0-169");
             }
 
             // workaround for job submission from win to linux -- https://issues.apache.org/jira/browse/MAPREDUCE-4052
@@ -113,10 +114,10 @@ public class KAPDebugTomcat {
             port = Integer.parseInt(args[0]);
         }
 
-        String webBase = new File("../../webapp/app").getAbsolutePath();
-        if (new File(webBase, "WEB-INF").exists() == false) {
-            throw new RuntimeException("In order to launch Kylin web app from IDE, please run:\ncp -rf extensions/server/src/main/webapp/WEB-INF webapp/app/");
-        }
+        File webBase = new File("../../webapp/app");
+        File webInfDir = new File(webBase, "WEB-INF");
+        FileUtils.deleteDirectory(webInfDir);
+        FileUtils.copyDirectoryToDirectory(new File("../server/src/main/webapp/WEB-INF"), webBase);
 
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(port);
@@ -127,7 +128,7 @@ public class KAPDebugTomcat {
         AprLifecycleListener listener = new AprLifecycleListener();
         server.addLifecycleListener(listener);
 
-        Context webContext = tomcat.addWebapp("/kylin", webBase);
+        Context webContext = tomcat.addWebapp("/kylin", webBase.getAbsolutePath());
         ErrorPage notFound = new ErrorPage();
         notFound.setErrorCode(404);
         notFound.setLocation("/index.html");
