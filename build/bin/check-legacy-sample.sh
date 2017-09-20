88f3ab4bd60258268a7eadb7bdc99c232ca44f06
auphyroc99 <454530524@qq.com>
2017-09-20 19:13:10 +0800

    #2405 Add a tool to remove legacy sample cube

--- build/bin/check-legacy-sample.sh
+++ build/bin/check-legacy-sample.sh
@@ -12,22 +12,12 @@ rm -rf ${_file}
 echo "Starting backup to ${_file}"
 mkdir -p ${_file}
 ${KYLIN_HOME}/bin/kylin.sh org.apache.kylin.common.persistence.ResourceTool fetch $_file cube_desc
-${KYLIN_HOME}/bin/kylin.sh org.apache.kylin.common.persistence.ResourceTool fetch $_file cube_instance
-${KYLIN_HOME}/bin/kylin.sh org.apache.kylin.common.persistence.ResourceTool fetch $_file raw_table_desc
-${KYLIN_HOME}/bin/kylin.sh org.apache.kylin.common.persistence.ResourceTool fetch $_file raw_table_instance
 echo "metadata store backed up to ${_file}"
 
 cd ${KYLIN_HOME}/meta_backups/meta_legacy/cube_desc
 sample_cube_name=`ls | grep kylin_sales_cube.json`
 if [ -n "$sample_cube_name" ] && [ "$sample_cube_name"="kylin_sales_cube.json" -a -z `awk '/version/{print}' $sample_cube_name` ]; then
-	cd ${KYLIN_HOME}/meta_backups/meta_legacy
-	rm -f ${KYLIN_HOME}/meta_backups/meta_legacy/cube*/kylin_sales_cube.json
-	rm -f ${KYLIN_HOME}/meta_backups/meta_legacy/raw_table*/kylin_sales_cube.json
-	${KYLIN_HOME}/bin/kylin.sh org.apache.kylin.common.persistence.ResourceTool remove cube_desc
-	${KYLIN_HOME}/bin/kylin.sh org.apache.kylin.common.persistence.ResourceTool remove cube_instance
-	${KYLIN_HOME}/bin/kylin.sh org.apache.kylin.common.persistence.ResourceTool remove raw_table_desc
-	${KYLIN_HOME}/bin/kylin.sh org.apache.kylin.common.persistence.ResourceTool remove raw_table_instance
-	bash ${KYLIN_HOME}/bin/metastore.sh restore ${KYLIN_HOME}/meta_backups/meta_legacy
+	${KYLIN_HOME}/bin/kylin.sh io.kyligence.kap.tool.release.KapCubeRemovalCLI kylin_sales_cube
 fi
 
 rm -rf ${_file}
