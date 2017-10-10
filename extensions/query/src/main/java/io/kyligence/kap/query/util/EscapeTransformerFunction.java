e87bbc20df0ee9c235d2cead4e3f1e7608b508d5
Yifan Zhang <event.dimlas@gmail.com>
2017-10-10 14:25:50 +0800

    #1510 EscapeTransformer for JDBC/ODBC function escape

---
+++ extensions/query/src/main/java/io/kyligence/kap/query/util/EscapeTransformerFunction.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2016 Kyligence Inc. All rights reserved.
+ *
+ * http://kyligence.io
+ *
+ * This software is the confidential and proprietary information of
+ * Kyligence Inc. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * Kyligence Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.kyligence.kap.query.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class EscapeTransformerFunction {
+
+    public static String transform(String functionName, String[] args) {
+        switch (functionName.toLowerCase()) {
+        /* String functions */
+        case "left":
+            return leftFN(args);
+        case "right":
+            return rightFN(args);
+        case "ltrim":
+            return ltrimFN(args);
+        case "rtrim":
+            return rtrimFN(args);
+        case "length":
+            return lengthFN(args);
+        case "convert":
+            return convertFN(args);
+        default:
+            return normalFN(functionName, args);
+        }
+
+    }
+
+    private static String normalFN(String functionName, String[] args) {
+        return new StringBuilder().append(functionName).append('(').append(StringUtils.join(args, ", ")).append(')')
+                .toString();
+    }
+
+    private static void checkArgs(String[] args, int expectedCount) {
+        int actualCount = 0;
+        if (args != null) {
+            actualCount = args.length;
+        }
+        if (actualCount != expectedCount) {
+            throw new IllegalArgumentException(
+                    "Bad arguments count, expected count " + expectedCount + " but actual " + actualCount);
+        }
+    }
+
+    private static String leftFN(String[] args) {
+        checkArgs(args, 2);
+        String[] newArgs = new String[] { args[0], "1", args[1] };
+        return normalFN("SUBSTRING", newArgs);
+    }
+
+    private static String rightFN(String[] args) {
+        checkArgs(args, 2);
+        String origStrRef = args[0];
+        Integer rightOffset = Integer.valueOf(args[1]);
+        String[] newArgs = new String[] { origStrRef, "CHAR_LENGTH(" + origStrRef + ") - " + (rightOffset - 1),
+                "" + rightOffset };
+        return normalFN("SUBSTRING", newArgs);
+    }
+
+    private static String ltrimFN(String[] args) {
+        checkArgs(args, 1);
+        String[] newArgs = new String[] { "leading " + args[0] };
+        return normalFN("TRIM", newArgs);
+    }
+
+    private static String rtrimFN(String[] args) {
+        checkArgs(args, 1);
+        String[] newArgs = new String[] { "trailing " + args[0] };
+        return normalFN("TRIM", newArgs);
+    }
+
+    private static String lengthFN(String[] args) {
+        checkArgs(args, 1);
+        return normalFN("CHAR_LENGTH", args);
+    }
+
+    private static String convertFN(String[] args) {
+        checkArgs(args, 2);
+        String value = args[0];
+        String sqlType = args[1].toUpperCase();
+        String sqlPrefix = "SQL_";
+        if (sqlType.startsWith(sqlPrefix)) {
+            sqlType = sqlType.substring(sqlPrefix.length());
+        }
+        String[] newArgs = new String[] { value + " AS " + sqlType };
+        return normalFN("CAST", newArgs);
+    }
+}
---
+++ extensions/query/src/test/java/io/kyligence/kap/query/util/EscapeTransformerTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2016 Kyligence Inc. All rights reserved.
+ *
+ * http://kyligence.io
+ *
+ * This software is the confidential and proprietary information of
+ * Kyligence Inc. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * Kyligence Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.kyligence.kap.query.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EscapeTransformerTest {
+
+    @Test
+    public void normalFNTest() {
+        String originalSQL = "select { fn count(*) }, avg(sales) from tbl";
+        String expectedSQL = "select count(*), avg(sales) from tbl";
+
+        EscapeTransformer transformer = new EscapeTransformer();
+        String transformedSQL = transformer.transform(originalSQL, null, null);
+        Assert.assertEquals(expectedSQL, transformedSQL);
+    }
+
+    @Test
+    public void leftFNTest() {
+        String originalSQL = "select { fn LEFT(LSTG_FORMAT_NAME, 2) } from KYLIN_SALES";
+        String expectedSQL = "select SUBSTRING(LSTG_FORMAT_NAME, 1, 2) from KYLIN_SALES";
+
+        EscapeTransformer transformer = new EscapeTransformer();
+        String transformedSQL = transformer.transform(originalSQL, null, null);
+        Assert.assertEquals(expectedSQL, transformedSQL);
+    }
+
+    @Test
+    public void rightFNTest() {
+        String originalSQL = "select { fn RIGHT(LSTG_FORMAT_NAME, 2) } from KYLIN_SALES";
+        String expectedSQL = "select SUBSTRING(LSTG_FORMAT_NAME, CHAR_LENGTH(LSTG_FORMAT_NAME) - 1, 2) from KYLIN_SALES";
+
+        EscapeTransformer transformer = new EscapeTransformer();
+        String transformedSQL = transformer.transform(originalSQL, null, null);
+        Assert.assertEquals(expectedSQL, transformedSQL);
+    }
+
+    @Test
+    public void lengthFNTest() {
+        String originalSQL = "select {fn LENGTH('Happy')}";
+        String expectedSQL = "select CHAR_LENGTH('Happy')";
+
+        EscapeTransformer transformer = new EscapeTransformer();
+        String transformedSQL = transformer.transform(originalSQL, null, null);
+
+        Assert.assertEquals(expectedSQL, transformedSQL);
+    }
+    
+    @Test
+    public void convertFNTest() {
+        String originalSQL = "select {fn CONVERT(PART_DT, SQL_DATE)}, {fn LTRIM({fn CONVERT(PRICE, SQL_VARCHAR)})} from KYLIN_SALES";
+        String expectedSQL = "select CAST(PART_DT AS DATE), TRIM(leading CAST(PRICE AS VARCHAR)) from KYLIN_SALES";
+
+        EscapeTransformer transformer = new EscapeTransformer();
+        String transformedSQL = transformer.transform(originalSQL, null, null);
+
+        Assert.assertEquals(expectedSQL, transformedSQL);
+    }
+}
