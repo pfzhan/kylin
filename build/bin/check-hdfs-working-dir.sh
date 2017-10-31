b1f9e205bf880ab375773c0f149b616faf3d80d1
Yifei Wu <vafuler945@gmail.com>
2017-10-31 18:55:09 +0800

    #2796 r/w naming refactor in check-hdfs-working-dir (#2985)

--- build/bin/check-hdfs-working-dir.sh
+++ build/bin/check-hdfs-working-dir.sh
@@ -21,27 +21,27 @@ else
 fi
 
 # test local hdfs
-## in read-write separation mode this is build cluster
-hadoop ${hadoop_conf_param} fs -test -d ${WORKING_DIR} || quit "ERROR: Please create working directory '${WORKING_DIR}' and grant access permission to current user."
+## in read-write separation mode this is write cluster
+hadoop ${hadoop_conf_param} fs -test -d ${WORKING_DIR} || quit "ERROR: Please create local working directory '${WORKING_DIR}' and grant access permission to current user."
 
 # test if kylin user (current user) has write permission to working directory
 touch ./${RANDNAME}
-hadoop ${hadoop_conf_param} fs -put -f ./${RANDNAME} ${TEST_FILE} || quit "ERROR: Have no permission to create/modify file in working directory '${WORKING_DIR}'. Please grant permission to current user."
+hadoop ${hadoop_conf_param} fs -put -f ./${RANDNAME} ${TEST_FILE} || quit "ERROR: Have no permission to create/modify file in local working directory '${WORKING_DIR}'. Please grant permission to current user."
 
 rm -f ./${RANDNAME}
 hadoop ${hadoop_conf_param} fs -rm -skipTrash ${TEST_FILE}
 
-# test remote hdfs if necessary
-# in read-write separation mode this is query cluster
+# test read hdfs if necessary
+# in read-write separation mode this is read cluster
 if [ -n ${ENABLE_FS_SEPARATE} ] && [ "${ENABLE_FS_SEPARATE}" == "true" ]; then
-    #convert local working dir to remote
-    remote_working_dir=$(${KYLIN_HOME}/bin/kylin.sh io.kyligence.kap.tool.setup.KapGetPathWithoutSchemeAndAuthorityCLI ${WORKING_DIR}| grep -v 'Usage'|tail -1)
-    remote_working_dir=`$KYLIN_HOME/bin/get-properties.sh kylin.storage.columnar.file-system`${remote_working_dir}
-    hadoop ${hadoop_conf_param} fs -test -d ${remote_working_dir} || quit "ERROR: Please create working directory '${remote_working_dir}' and grant access permission to current user."
+    #convert local working dir to read cluster working dir
+    read_working_dir=$(${KYLIN_HOME}/bin/kylin.sh io.kyligence.kap.tool.setup.KapGetPathWithoutSchemeAndAuthorityCLI ${WORKING_DIR}| grep -v 'Usage'|tail -1)
+    read_working_dir=`$KYLIN_HOME/bin/get-properties.sh kylin.storage.columnar.file-system`${read_working_dir}
+    hadoop ${hadoop_conf_param} fs -test -d ${read_working_dir} || quit "ERROR: Please create read cluster working directory '${read_working_dir}' and grant access permission to current user."
 
     touch ./${RANDNAME}
-    TEST_FILE=${remote_working_dir}/${RANDNAME}
-    hadoop ${hadoop_conf_param} fs -put -f ./${RANDNAME} ${TEST_FILE} || quit "ERROR: Have no permission to create/modify file in working directory '${remote_working_dir}'. Please grant permission to current user."
+    TEST_FILE=${read_working_dir}/${RANDNAME}
+    hadoop ${hadoop_conf_param} fs -put -f ./${RANDNAME} ${TEST_FILE} || quit "ERROR: Have no permission to create/modify file in read cluster working directory '${read_working_dir}'. Please grant permission to current user."
 
     rm -f ./${RANDNAME}
     hadoop ${hadoop_conf_param} fs -rm -skipTrash ${TEST_FILE}
