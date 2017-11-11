68d895c3f7a45e76d0b495519cbd534a39431d4f
nichunen <chunen.ni@kyligence.io>
2017-11-11 22:29:36 +0800

    Add option for skipping npm install in build.sh

--- build/script/build.sh
+++ build/script/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 dir=$(dirname ${0})
+install_front=false
+if [[ ${1} == "skip-install-front" ]]
+then
+    echo 'Skip install front-end dependencies'
+    install_front=true
+    shift
+fi
+
 cd ${dir}/../..
 
 echo 'Build back-end'
@@ -9,5 +17,9 @@ mvn clean install -DskipTests $@ || { exit 1; }
 #package webapp
 echo 'Build front-end'
 cd kystudio
-npm install						 || { exit 1; }
+if [[ ${install_front} = false ]]
+then
+    echo 'Install front-end dependencies'
+    npm install						 || { exit 1; }
+fi
 npm run build		 || { exit 1; }
