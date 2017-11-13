2cd255bc5a5c2540f61b47903c0bece983db091e
nichunen <chunen.ni@kyligence.io>
2017-11-13 10:22:33 +0800

    Rename "install_front" to "skip_front"

--- build/script/build.sh
+++ build/script/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 dir=$(dirname ${0})
-install_front=false
+skip_front=false
 if [[ ${1} == "skip-install-front" ]]
 then
     echo 'Skip install front-end dependencies'
-    install_front=true
+    skip_front=true
     shift
 fi
 
@@ -17,7 +17,7 @@ mvn clean install -DskipTests $@ || { exit 1; }
 #package webapp
 echo 'Build front-end'
 cd kystudio
-if [[ ${install_front} = false ]]
+if [[ ${skip_front} = false ]]
 then
     echo 'Install front-end dependencies'
     npm install						 || { exit 1; }
