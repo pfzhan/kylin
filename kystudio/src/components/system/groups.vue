9bf574e5103031fd034cf42495bcf95880a13699
luguosheng1314 <550175214@qq.com>
2017-11-17 20:38:11 -0600

    #3255 locale (#3256)

--- kystudio/src/components/system/groups.vue
+++ kystudio/src/components/system/groups.vue
@@ -281,7 +281,7 @@ export default {
   },
   locales: {
     'en': {newGroup: 'New Group', assignUsers: 'Assign Users', willcheck: 'Unassigned Users', haschecked: 'Assigned Users', confirmDelGroup: 'Are you sure to delete this group?', usersCount: 'User Number'},
-    'zh-cn': {newGroup: '新的组', assignUsers: '分配用户', willcheck: '未分配的用户', haschecked: '已分配组用户', confirmDelGroup: '确认删除该组吗？', usersCount: '用户数'}
+    'zh-cn': {newGroup: '新建组', assignUsers: '分配用户', willcheck: '未分配的用户', haschecked: '已分配组用户', confirmDelGroup: '确认删除该组吗？', usersCount: '用户数'}
   }
 }
 </script>
