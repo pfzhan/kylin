b06903c9a8d009d65c2051b8ec2fbd07a5155e57
Pan Wang <pan.wang@kyligence.io>
2018-11-21 16:32:23 +0800

    #8409 customized spark which replace parquet jar

--- build/script_newten/download-spark.sh
+++ build/script_newten/download-spark.sh
@@ -7,25 +7,26 @@ source build/script_newten/functions.sh
 
 rm -rf build/spark
 
-spark_version="2.2.1"
-spark_pkg_md5="7774419f0fb7c28d93c24aefb19a014c"
+spark_pkg_name="spark-newten-2.2.1"
+spark_pkg_file_name="${spark_pkg_name}.tgz"
+spark_pkg_md5="6c300138d1ecf7423fd12a3e7bcadbb8"
 
-if [ ! -f "build/spark-${spark_version}-bin-hadoop2.6.tgz" ]
+if [ ! -f "build/${spark_pkg_file_name}" ]
 then
     echo "no binary file found"
-    wget --directory-prefix=build/ http://archive.apache.org/dist/spark/spark-${spark_version}/spark-${spark_version}-bin-hadoop2.6.tgz || echo "Download spark failed"
+    wget --directory-prefix=build/ https://s3.us-east-2.amazonaws.com/download-resource/kyspark/${spark_pkg_file_name} || echo "Download spark failed"
 else
-    if [ `calMd5 build/spark-${spark_version}-bin-hadoop2.6.tgz | awk '{print $1}'` != "${spark_pkg_md5}" ]
+    if [ `calMd5 build/${spark_pkg_file_name} | awk '{print $1}'` != "${spark_pkg_md5}" ]
     then
         echo "md5 check failed"
-        rm build/spark-${spark_version}-bin-hadoop2.6.tgz
-        wget --directory-prefix=build/ http://archive.apache.org/dist/spark/spark-${spark_version}/spark-${spark_version}-bin-hadoop2.6.tgz || echo "Download spark failed"
+        rm build/${spark_pkg_file_name}
+        wget --directory-prefix=build/ https://s3.us-east-2.amazonaws.com/download-resource/kyspark/${spark_pkg_file_name} || echo "Download spark failed"
 
     fi
 fi
 
-tar -zxvf build/spark-${spark_version}-bin-hadoop2.6.tgz -C build/   || { exit 1; }
-mv build/spark-${spark_version}-bin-hadoop2.6 build/spark
+tar -zxvf build/${spark_pkg_file_name} -C build/   || { exit 1; }
+mv build/${spark_pkg_name} build/spark
 
 # Remove unused components in Spark
 rm -rf build/spark/lib/spark-examples-*
