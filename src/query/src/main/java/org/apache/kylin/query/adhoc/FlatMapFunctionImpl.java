9691fd615d8f645b14d5e7eeeb9a4beb51dbd035
Hongbin Ma <ma@kyligence.io>
2018-12-07 20:31:40 +0800

    refactor: remove useless modules

---
+++ src/query/src/main/java/org/apache/kylin/query/adhoc/FlatMapFunctionImpl.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2016 Kyligence Inc. All rights reserved.
+ *
+ * http://kyligence.io
+ *
+ * This software is the confidential and proprietary information of
+ * Kyligence Inc. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * Kyligence Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.apache.kylin.query.adhoc;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.Row;
+
+import com.google.common.collect.Lists;
+
+import io.kyligence.kap.common.obf.IKeepNames;
+
+public class FlatMapFunctionImpl implements IKeepNames, FlatMapFunction<Iterator<Row>, List<String>> {
+    public static final long serialVersionUID = 11L;
+
+    @Override
+    public Iterator<List<String>> call(Iterator<Row> iterator) throws Exception {
+        List<List<String>> rowList = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            List<String> data = Lists.newArrayList();
+            Row curRow = iterator.next();
+
+            for (int i = 0; i < curRow.length(); i++) {
+                Object obj = curRow.getAs(i);
+                if (null == obj) {
+                    data.add(null);
+                } else {
+                    data.add(obj.toString());
+                }
+            }
+            rowList.add(data);
+        }
+        return rowList.iterator();
+    }
+}
