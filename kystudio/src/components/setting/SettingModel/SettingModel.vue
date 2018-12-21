00e1917a39f92df8328e6baec52195992cda596c
Qian Xia <lauraxiaqian@gmail.com>
2018-12-21 15:45:27 +0800

    #9299 setting model UI (#9316)

--- kystudio/src/components/setting/SettingModel/SettingModel.vue
+++ kystudio/src/components/setting/SettingModel/SettingModel.vue
@@ -14,14 +14,14 @@
       <el-table-column min-width="400px" :label="$t('modelSetting')">
         <template slot-scope="scope">
           <div v-if="scope.row.segmentMerge&&scope.row.segmentMerge.length">
-            <span class="model-setting-item">
-              {{$t('segmentMerge')}}<span v-for="item in scope.row.segmentMerge" :key="item">{{item}}</span>
+            <span class="model-setting-item" @click="editMergeItem(scope.row)">
+              {{$t('segmentMerge')}}<span v-for="item in scope.row.segmentMerge" :key="item">{{$t(item)}}</span>
             </span>
             <i class="el-icon-ksd-symbol_type"></i>
           </div>
           <div v-if="scope.row.retention">
-            <span class="model-setting-item">
-              {{$t('retention')}}<span>{{scope.row.retention}}</span>
+            <span class="model-setting-item" @click="editRetentionItem(scope.row)">
+              {{$t('retention')}}<span>{{scope.row.retention.value}} {{scope.row.retention.unit}}</span>
             </span>
             <i class="el-icon-ksd-symbol_type"></i>
           </div>
@@ -32,10 +32,60 @@
         align="center"
         :label="$t('kylinLang.common.action')">
           <template slot-scope="scope">
-            <i class="el-icon-ksd-table_add"></i>
+            <i class="el-icon-ksd-table_add" @click="addSettingItem(scope.row)"></i>
           </template>
       </el-table-column>
     </el-table>
+    <el-dialog :title="modelSettingTitle" :visible.sync="editModelSetting" width="440px" class="model-setting-dialog">
+      <el-form label-position="top" size="medium" label-width="80px" :model="modelSettingForm">
+        <el-form-item :label="$t('modelName')">
+          <el-input v-model.trim="modelSettingForm.name" disabled></el-input>
+        </el-form-item>
+        <el-form-item :label="$t('settingItem')" v-if="step=='stepOne'">
+          <el-select v-model="modelSettingForm.settingItem" size="medium" :placeholder="$t('kylinLang.common.pleaseSelect')" style="width:100%">
+            <el-option
+              v-for="item in settingOption"
+              :key="item"
+              :label="item"
+              :value="item">
+            </el-option>
+          </el-select>
+          <p v-if="modelSettingForm.settingItem==='Auto-merge'">Segment auto-merge can help defragment your data file by merging the small segments into medium and large segment automatically.</p>
+          <p v-if="modelSettingForm.settingItem==='Retention Threshold'">Only keep the segment whose data is in past given days in cube, the old segment will be automatically dropped from head. </p>
+        </el-form-item>
+        <el-form-item :label="$t('autoMerge')" class="ksd-mb-10" v-if="step=='stepTwo'&&modelSettingForm.settingItem==='Auto-merge'">
+          <el-checkbox-group v-model="modelSettingForm.autoMerge" class="merge-groups">
+            <div><el-checkbox v-for="(item, index) in mergeGroups" :label="item" :key="item" v-if="index<3">{{$t(item)}}</el-checkbox></div>
+            <div><el-checkbox v-for="(item, index) in mergeGroups" :label="item" :key="item" v-if="index>2">{{$t(item)}}</el-checkbox></div>
+          </el-checkbox-group>
+        </el-form-item>
+        <el-form-item :label="$t('retentionThreshold')" v-if="step=='stepTwo'&&modelSettingForm.settingItem==='Retention Threshold'">
+          <el-input v-model.trim="modelSettingForm.retentionThreshold.value" class="retention-input"></el-input>
+          <el-select v-model="modelSettingForm.retentionThreshold.unit" class="ksd-ml-8" size="medium" :placeholder="$t('kylinLang.common.pleaseSelect')">
+            <el-option
+              v-for="item in units"
+              :key="item"
+              :label="$t(item)"
+              :value="item">
+            </el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editModelSetting = false" size="medium" v-if="step=='stepOne'">{{$t('kylinLang.common.cancel')}}</el-button>
+        <el-button @click="preStep" icon="el-icon-ksd-more_01-copy" size="medium" v-if="step=='stepTwo'">{{$t('kylinLang.common.prev')}}</el-button>
+        <el-button type="primary" plain @click="nextStep" size="medium" v-if="step=='stepOne'" :disabled="modelSettingForm.settingItem==''">{{$t('kylinLang.common.next')}}<i class="el-icon-ksd-more_02 el-icon--right"></i></el-button>
+        <el-button
+          type="primary"
+          plain
+          @click="submit"
+          size="medium"
+          v-if="step=='stepTwo'"
+          :disabled="isSubmit">
+            {{$t('kylinLang.common.submit')}}
+          </el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -66,13 +116,63 @@ import { transToGmtTime } from '../../../util/business'
 })
 export default class SettingStorage extends Vue {
   modelList = [
-    {name: 'nmodel_basic_inner', last_modified: 1545188124251, owner: 'ADMIN', segmentMerge: ['One day', 'One week'], retention: '200 days'},
-    {name: 'nmodel_full_measure_test', last_modified: 1545188124251, owner: 'ADMIN', segmentMerge: ['One day', 'One week'], retention: '200 days'},
-    {name: 'test_encoding', last_modified: 1545188124251, owner: 'ADMIN', segmentMerge: ['One day', 'One week'], retention: '200 days'},
-    {name: 'ut_inner_join_cube_partial', last_modified: 1545188124251, owner: 'ADMIN', segmentMerge: ['One day', 'One week'], retention: '200 days'},
-    {name: 'nmodel_basic', last_modified: 1545188124251, owner: 'ADMIN', segmentMerge: ['One day', 'One week'], retention: '200 days'},
-    {name: 'all_fixed_length', last_modified: 1545188124251, owner: 'ADMIN', segmentMerge: ['One day', 'One week'], retention: '200 days'}
+    {name: 'nmodel_basic_inner', last_modified: 1545188124251, owner: 'ADMIN', segmentMerge: ['HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR'], retention: {value: 200, unit: 'days'}},
+    {name: 'nmodel_full_measure_test', last_modified: 1545188124251, owner: 'ADMIN', segmentMerge: ['HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR'], retention: {value: 200, unit: 'days'}},
+    {name: 'test_encoding', last_modified: 1545188124251, owner: 'ADMIN', segmentMerge: ['HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR'], retention: {value: 200, unit: 'days'}},
+    {name: 'ut_inner_join_cube_partial', last_modified: 1545188124251, owner: 'ADMIN', segmentMerge: ['HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR'], retention: {value: 200, unit: 'days'}},
+    {name: 'nmodel_basic', last_modified: 1545188124251, owner: 'ADMIN', segmentMerge: ['HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR'], retention: {value: 200, unit: 'days'}},
+    {name: 'all_fixed_length', last_modified: 1545188124251, owner: 'ADMIN', segmentMerge: ['HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR'], retention: {value: 200, unit: 'days'}}
   ]
+  editModelSetting = false
+  isEdit = false
+  step = 'stepOne'
+  settingOption = ['Auto-merge', 'Retention Threshold']
+  mergeGroups = ['HOUR', 'DAY', 'WEEK', 'MONTH', 'YEAR']
+  units = ['day', 'week', 'year']
+  modelSettingForm = {name: '', settingItem: '', autoMerge: [], retentionThreshold: {value: 0, unit: ''}}
+
+  addSettingItem (row) {
+    this.modelSettingForm.name = row.name
+    this.editModelSetting = true
+  }
+  get modelSettingTitle () {
+    return this.isEdit ? this.$t('editSetting') : this.$t('newSetting')
+  }
+  get isSubmit () {
+    if (this.modelSettingForm.settingItem === 'Auto-merge' && !this.modelSettingForm.autoMerge.length) {
+      return true
+    } else if (this.modelSettingForm.settingItem === 'Retention Threshold' && (!this.modelSettingForm.retentionThreshold.value || !this.modelSettingForm.retentionThreshold.unit)) {
+      return true
+    } else {
+      return false
+    }
+  }
+  nextStep () {
+    this.step = 'stepTwo'
+  }
+  preStep () {
+    this.step = 'stepOne'
+  }
+  editMergeItem (row) {
+    this.modelSettingForm.name = row.name
+    this.modelSettingForm.settingItem = 'Auto-merge'
+    this.$nextTick(() => {
+      debugger
+      this.modelSettingForm.autoMerge = JSON.parse(JSON.stringify(row.segmentMerge))
+    })
+    this.step = 'stepTwo'
+    this.isEdit = true
+    this.editModelSetting = true
+  }
+  editRetentionItem (row) {
+    this.modelSettingForm.name = row.name
+    this.modelSettingForm.settingItem = 'Retention Threshold'
+    this.modelSettingForm.retentionThreshold = JSON.parse(JSON.stringify(row.retention))
+    this.step = 'stepTwo'
+    this.isEdit = true
+    this.editModelSetting = true
+  }
+  submit () {}
   created () {
   }
 }
@@ -91,6 +191,31 @@ export default class SettingStorage extends Vue {
       color: @base-color;
       cursor: pointer;
     }
+  }
+}
+.model-setting-dialog {
+  .el-form-item__content p {
+    font-size: 12px;
+    line-height: 16px;
+    color: @text-normal-color;
+    margin-top: 5px;
+  }
+  .merge-groups {
+    > div:nth-child(2) {
+      margin-top: -15px;
+    }
+    .el-checkbox {
+      &+.el-checkbox {
+        margin-left: 30px;
+      }
+      &:nth-child(4) {
+        margin-left: 0;
+      }
+    }
+  }
+  .retention-input {
+    display: inline-block;
+    width: 100px;
   }
 }
 </style>
