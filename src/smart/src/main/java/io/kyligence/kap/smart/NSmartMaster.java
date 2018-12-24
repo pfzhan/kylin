21c9b4c29f8c8c3c4a7251041936dabb73748cde
Yifan Zhang <event.dimlas@gmail.com>
2018-12-24 14:17:23 +0800

    #9344 minor fix, use threadLocal KylinConfig within UnitOfWork

--- src/smart/src/main/java/io/kyligence/kap/smart/NSmartMaster.java
+++ src/smart/src/main/java/io/kyligence/kap/smart/NSmartMaster.java
@@ -54,24 +54,18 @@ public class NSmartMaster {
 
     private NSmartContext context;
     private NProposerProvider proposerProvider;
-    private NDataModelManager dataModelManager;
-    FavoriteQueryManager favoriteQueryManager;
     private String project;
 
     public NSmartMaster(KylinConfig kylinConfig, String project, String[] sqls) {
         this.project = project;
         this.context = new NSmartContext(kylinConfig, project, sqls);
         this.proposerProvider = NProposerProvider.create(context);
-        this.dataModelManager = NDataModelManager.getInstance(kylinConfig, project);
-        this.favoriteQueryManager = FavoriteQueryManager.getInstance(kylinConfig, project);
     }
 
     public NSmartMaster(KylinConfig kylinConfig, String project, String[] sqls, String draftVersion) {
         this.project = project;
         this.context = new NSmartContext(kylinConfig, project, sqls, draftVersion);
         this.proposerProvider = NProposerProvider.create(context);
-        this.dataModelManager = NDataModelManager.getInstance(kylinConfig, project);
-        this.favoriteQueryManager = FavoriteQueryManager.getInstance(kylinConfig, project);
     }
 
     public NSmartContext getContext() {
@@ -107,6 +101,7 @@ public class NSmartMaster {
     }
 
     public void renameModel() {
+        NDataModelManager dataModelManager = NDataModelManager.getInstance(KylinConfig.getInstanceFromEnv(), project);
         List<NDataModel> modelList = dataModelManager.getDataModels();
         Set<String> usedNames = Sets.newHashSet();
         if (modelList != null) {
@@ -168,11 +163,11 @@ public class NSmartMaster {
         analyzeSQLs();
         selectModel();
         optimizeModel();
-        renameModel();
         selectCubePlan();
         optimizeCubePlan();
 
         UnitOfWork.doInTransactionWithRetry(() -> {
+            renameModel();
             saveModel();
             saveCubePlan();
             saveAccelerateInfo();
@@ -184,8 +179,10 @@ public class NSmartMaster {
     }
 
     public void saveCubePlan() {
-        NDataflowManager dataflowManager = NDataflowManager.getInstance(context.getKylinConfig(), context.getProject());
-        NCubePlanManager cubePlanManager = NCubePlanManager.getInstance(context.getKylinConfig(), context.getProject());
+        NDataflowManager dataflowManager = NDataflowManager.getInstance(KylinConfig.getInstanceFromEnv(),
+                context.getProject());
+        NCubePlanManager cubePlanManager = NCubePlanManager.getInstance(KylinConfig.getInstanceFromEnv(),
+                context.getProject());
         for (NSmartContext.NModelContext modelCtx : context.getModelContexts()) {
             if (modelCtx.withoutTargetModel()) {
                 continue;
@@ -194,32 +191,34 @@ public class NSmartMaster {
             if (cubePlanManager.getCubePlan(cubePlan.getName()) == null) {
                 cubePlanManager.createCubePlan(cubePlan);
                 dataflowManager.createDataflow(cubePlan.getName(), cubePlan, cubePlan.getModel().getOwner());
-                continue;
+            } else {
+                cubePlanManager.updateCubePlan(cubePlan);
             }
-
-            cubePlanManager.updateCubePlan(cubePlan);
         }
     }
 
     public void saveAccelerateInfo() {
+        FavoriteQueryManager favoriteQueryManager = FavoriteQueryManager.getInstance(KylinConfig.getInstanceFromEnv(),
+                project);
         val accelerateInfoMap = context.getAccelerateInfoMap();
         accelerateInfoMap.forEach((sqlPattern, accelerateInfo) -> {
-            if (!accelerateInfo.isBlocked()) {
-                List<FavoriteQueryRealization> favoriteQueryRealizations = Lists.newArrayList();
-                for (val layout : accelerateInfo.getRelatedLayouts()) {
-                    FavoriteQueryRealization realization = new FavoriteQueryRealization();
-                    realization.setModelId(layout.getModelId());
-                    realization.setCubePlanId(layout.getCubePlanId());
-                    realization.setCuboidLayoutId(layout.getLayoutId());
-                    favoriteQueryRealizations.add(realization);
-                }
-
-                favoriteQueryManager.resetRealizations(sqlPattern, favoriteQueryRealizations);
+            if (accelerateInfo.isBlocked()) {
+                return;
             }
+            List<FavoriteQueryRealization> favoriteQueryRealizations = Lists.newArrayList();
+            for (val layout : accelerateInfo.getRelatedLayouts()) {
+                FavoriteQueryRealization realization = new FavoriteQueryRealization();
+                realization.setModelId(layout.getModelId());
+                realization.setCubePlanId(layout.getCubePlanId());
+                realization.setCuboidLayoutId(layout.getLayoutId());
+                favoriteQueryRealizations.add(realization);
+            }
+            favoriteQueryManager.resetRealizations(sqlPattern, favoriteQueryRealizations);
         });
     }
 
     public void saveModel() {
+        NDataModelManager dataModelManager = NDataModelManager.getInstance(KylinConfig.getInstanceFromEnv(), project);
         for (NSmartContext.NModelContext modelCtx : context.getModelContexts()) {
             if (modelCtx.withoutTargetModel()) {
                 continue;
