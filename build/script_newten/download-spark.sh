8b3c516867a502d12007aacfd74b148be2f6ab3a
yiming.xu <100650920@qq.com>
2019-01-25 22:16:45 +0800

    minor fix spark md5 error

--- build/script_newten/download-spark.sh
+++ build/script_newten/download-spark.sh
@@ -7,9 +7,9 @@ source build/script_newten/functions.sh
 
 rm -rf build/spark
 
-spark_pkg_name="spark-newten-2.4.1-r2"
+spark_pkg_name="spark-newten-2.4.1"
 spark_pkg_file_name="${spark_pkg_name}.tgz"
-spark_pkg_md5="2d12f1790b4bf6eecd196d638db7245d"
+spark_pkg_md5="7b101d45081e3281016e849a39920fae"
 
 if [ ! -f "build/${spark_pkg_file_name}" ]
 then
