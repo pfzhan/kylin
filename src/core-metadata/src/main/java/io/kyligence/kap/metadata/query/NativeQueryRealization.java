2a26f20366fd5862945e9a9540c0fb09985ce387
Xinbei Fu <23435773+Xinbei@users.noreply.github.com>
2019-04-08 10:26:03 +0800

    minor, fix sonar

--- src/core-metadata/src/main/java/io/kyligence/kap/metadata/query/NativeQueryRealization.java
+++ src/core-metadata/src/main/java/io/kyligence/kap/metadata/query/NativeQueryRealization.java
@@ -29,11 +29,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class NativeQueryRealization {
+public class NativeQueryRealization implements Serializable {
     private String modelId;
     private String modelAlias;
     private Long layoutId;
