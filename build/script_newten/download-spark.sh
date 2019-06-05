346aeb1972746bdddaf46009d5bf3829e2902c31
Mingming Ge <mingming.ge@kyligence.io>
2019-06-05 21:15:51 +0800

    #12025 release spark spark-newten-2.4.1-r6 (#12141)

--- build/script_newten/download-spark.sh
+++ build/script_newten/download-spark.sh
@@ -7,9 +7,9 @@ source build/script_newten/functions.sh
 
 rm -rf build/spark
 
-spark_pkg_name="spark-newten-2.4.1-r5"
+spark_pkg_name="spark-newten-2.4.1-r6"
 spark_pkg_file_name="${spark_pkg_name}.tgz"
-spark_pkg_md5="688f23bc2ded8fa82f6a55093d4a804e"
+spark_pkg_md5="1181ee03baf2f67400c2fb974c6a65cb"
 
 if [ ! -f "build/${spark_pkg_file_name}" ]
 then
