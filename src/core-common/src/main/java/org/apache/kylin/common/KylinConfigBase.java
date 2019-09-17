6f3fa7ded1fd0ad007641d8bc3b06329c7ac51ff
ffbh <2529026882@qq.com>
2019-09-17 13:49:35 +0800

    #14563 Configs for allowing export query result for admin/nonadmin user

--- src/core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
+++ src/core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
@@ -1622,7 +1622,7 @@ abstract public class KylinConfigBase implements Serializable {
                 "kylin.web.timezone,kylin.query.cache-enabled,kylin.env,kylin.web.hive-limit,kylin.storage.default,kylin.engine.default,kylin.web.link-hadoop,kylin.web.link-diagnostic,"
                         + "kylin.web.contact-mail,kylin.web.help.length,kylin.web.help.0,kylin.web.help.1,kylin.web.help.2,kylin.web.help.3,"
                         + "kylin.web.help,kylin.web.hide-measures,kylin.web.link-streaming-guide,kylin.server.external-acl-provider,kylin.security.profile,"
-                        + "kylin.htrace.show-gui-trace-toggle");
+                        + "kylin.htrace.show-gui-trace-toggle,kylin.web.export-allow-admin,kylin.web.export-allow-other");
     }
 
     public boolean getEventAutoApproved() {
