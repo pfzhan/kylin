f3f3d5bc489b226672a23b1c61b08c15b035df32
yiming.xu <100650920@qq.com>
2019-11-08 16:17:04 +0800

    #15841 optimize snappy decompress

--- build/script_newten/download-spark.sh
+++ build/script_newten/download-spark.sh
@@ -7,9 +7,9 @@ source build/script_newten/functions.sh
 
 rm -rf build/spark
 
-spark_pkg_name="spark-newten-2.4.1-r18"
+spark_pkg_name="spark-newten-2.4.1-r19"
 spark_pkg_file_name="${spark_pkg_name}.tgz"
-spark_pkg_md5="3f77d24d5b3e821fdfa6707d30571592"
+spark_pkg_md5="33ede85f28869a88861eb60602b730d0"
 
 if [ ! -f "build/${spark_pkg_file_name}" ]
 then
