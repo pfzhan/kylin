0506a60b44dc4043fefc6ac0a1967a0e02d7f68b
lxian2shell <lxian2shell@gmail.com>
2019-12-23 11:54:02 +0800

    #16104 correct project to window rule cohorts (#16989)

---
+++ src/kap-it/src/test/resources/query/sql_window/new_sql_window/query21.sql
@@ -0,0 +1,22 @@
+-- KAP#16104
+-- nested window
+SELECT
+	lag(c_pct_x, 1, 0) OVER (
+		PARTITION BY tag ORDER BY sort_dp
+		) AS lagx
+FROM (
+	SELECT tag
+				,y / x AS sort_dp
+		,
+		(
+			sum(x) OVER (
+				PARTITION BY tag ORDER BY y / x
+				)
+			) / (sum(x) OVER (PARTITION BY tag)) AS c_pct_x
+	FROM (
+				SELECT 'model' AS tag
+					,item_count AS x
+					,price AS y
+				FROM TEST_KYLIN_FACT
+		) tb1
+	)
