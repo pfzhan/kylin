017c7e65f7c84fdd4d2677e688036776c8053d0d
ffbh <2529026882@qq.com>
2020-01-16 17:16:14 +0800

    KE-9489 add license check filter

---
+++ src/server-base/src/main/java/io/kyligence/kap/rest/interceptor/LicenseFilter.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2016 Kyligence Inc. All rights reserved.
+ *
+ * http://kyligence.io
+ *
+ * This software is the confidential and proprietary information of
+ * Kyligence Inc. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * Kyligence Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package io.kyligence.kap.rest.interceptor;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.kylin.rest.exception.UnauthorizedException;
+import org.apache.kylin.rest.service.LicenseInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import lombok.val;
+
+@Component
+@Order(-500)
+public class LicenseFilter implements Filter {
+
+    private static final String PREFIX = "/kylin/api/";
+    private static String[] apiWhiteList = { "config", "system", "error" };
+
+    @Autowired
+    private LicenseInfoService licenseInfoService;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // just override it
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest servletRequest = (HttpServletRequest) request;
+            boolean noNeedLicenseCheck = Arrays.stream(apiWhiteList).map(white -> PREFIX + white)
+                    .anyMatch(api -> servletRequest.getRequestURI().startsWith(api));
+            if (!noNeedLicenseCheck) {
+                try {
+                    val info = licenseInfoService.extractLicenseInfo();
+                    licenseInfoService.verifyLicense(info);
+                } catch (Exception e) {
+                    servletRequest.setAttribute("error", new UnauthorizedException(e.getMessage()));
+                    servletRequest.getRequestDispatcher("/api/error").forward(servletRequest, response);
+                    return;
+                }
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // just override it
+    }
+}
