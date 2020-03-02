649554d8d7544f1ac3c89ee7797f72d8fac67f14
xifeng yang <xifeng.yang@hotmail.com>
2020-03-02 17:23:05 +0800

    KE-12119 throw exception while v2 api was not support

---
+++ src/server-base/src/main/java/io/kyligence/kap/rest/controller/CommonControllerAdvice.java
@@ -0,0 +1,55 @@
+package io.kyligence.kap.rest.controller;
+/*
+ * Copyright (C) 2016 Kyligence Inc. All rights reserved.
+ *
+ * http://kyligence.io
+ *
+ * This software is the confidential and proprietary information of
+ * Kyligence Inc. ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * Kyligence Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import java.util.Objects;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.kylin.rest.exception.NotFoundException;
+import org.apache.kylin.rest.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import io.kyligence.kap.common.http.HttpConstant;
+
+@ControllerAdvice
+public class CommonControllerAdvice {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = { NoHandlerFoundException.class, HttpMediaTypeNotAcceptableException.class })
+    @ResponseBody
+    ErrorResponse noHandlerFoundException(HttpServletRequest req, Exception ex) {
+        if (Objects.equals(req.getHeader("Accept"), HttpConstant.HTTP_VND_APACHE_KYLIN_V2_JSON)) {
+            String uri = req.getRequestURI();
+            ex = new NotFoundException(String.format("%s API of version v2 is no longer supported", uri));
+        }
+        return new ErrorResponse(req.getRequestURL().toString(), ex);
+    }
+}
