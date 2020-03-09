32aab277d383bd8a118ccc8ca5a605a3e4c4ea63
Enwei Jiao <enwei.jiao@kyligence.io>
2020-03-09 15:48:08 +0800

    use npm instead (#18378)

--- build/script_newten/build.sh
+++ build/script_newten/build.sh
@@ -13,10 +13,11 @@ cd kystudio
 if [ "${SKIP_FRONT}" = "0" ];
 then
     echo 'Install front-end dependencies'
-    if ! [[ -x "$(command -v cnpm)" ]]; then
-        npm install -g cnpm --registry=https://registry.npm.taobao.org  || { exit 1; }
-    fi
-    cnpm install						 || { exit 1; }
+#    if ! [[ -x "$(command -v cnpm)" ]]; then
+#        npm install -g cnpm --registry=https://registry.npm.taobao.org  || { exit 1; }
+#    fi
+#    cnpm install						 || { exit 1; }
+    npm install
 fi
 
 npm run build		 || { exit 1; }
