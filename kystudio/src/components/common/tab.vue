4e746c315595055dc3e9f6a14655fa307bdf61ad
ChunYan Huang <qingyanxiaonian@163.com>
2020-03-11 05:27:13 -0500

    KE-12643,fix query tab icon bug (#18439)

--- kystudio/src/components/common/tab.vue
+++ kystudio/src/components/common/tab.vue
@@ -3,7 +3,7 @@
   <el-tabs v-model="activeName" :type="type" :editable="editable" :addable="false" @tab-click="handleClick" @edit="handleTabsEdit">
     <slot name="defaultPane"></slot>
     <el-tab-pane
-      v-for="item in tabs" :key="item.name"
+      v-for="item in tabs" :key="item.name + item.icon"
       :label="item.i18n? item.title.replace(item.i18n,$t('kylinLang.common.'+item.i18n)): item.title"
       :name="item.name"
       :closable="item.closable">
