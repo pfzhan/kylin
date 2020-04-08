0d6b5231b0e11c84179bf95e9d21e85127836d63
JiaZhi Zhou <jiazhi.zhou@kyligence.io>
2020-04-08 10:21:40 +0800

    Revert "KE-12515 fix restore bug"
    This reverts commit 363bf0a279a787d1bc11641025b53de6e6998651.

--- src/core-common/src/test/java/org/apache/kylin/common/persistence/ResourceStoreTest.java
+++ src/core-common/src/test/java/org/apache/kylin/common/persistence/ResourceStoreTest.java
@@ -73,34 +73,4 @@ public class ResourceStoreTest extends NLocalFileMetadataTestCase {
         res = ResourceStore.getKylinMetaStore(KylinConfig.getInstanceFromEnv()).listResources("/");
         Assert.assertTrue("failed", res.contains(ResourceStore.METASTORE_UUID_TAG));
     }
-
-    @Test
-    public void testCreateMetaStoreUuidUsingTwoKylinConfig() {
-        KylinConfig A = KylinConfig.getInstanceFromEnv();
-        ResourceStore AStore = ResourceStore.getKylinMetaStore(A);//system store
-        KylinConfig B = KylinConfig.createKylinConfig(A);
-        ResourceStore BStore = ResourceStore.getKylinMetaStore(B);//new store
-
-        //delete AStore,BStore uuid
-        ResourceStore.getKylinMetaStore(A).createMetaStoreUuidIfNotExist();
-        ResourceStore.getKylinMetaStore(A).deleteResource(ResourceStore.METASTORE_UUID_TAG);
-        ResourceStore.getKylinMetaStore(B).createMetaStoreUuidIfNotExist();
-        ResourceStore.getKylinMetaStore(B).deleteResource(ResourceStore.METASTORE_UUID_TAG);
-        Set<String> res = AStore.listResources("/");
-        Assert.assertFalse(res.contains(ResourceStore.METASTORE_UUID_TAG));
-        res = BStore.listResources("/");
-        Assert.assertFalse(res.contains(ResourceStore.METASTORE_UUID_TAG));
-
-        //create BStore uuid
-        BStore.createMetaStoreUuidIfNotExist();
-        res = BStore.listResources("/");
-        Assert.assertTrue(res.contains(ResourceStore.METASTORE_UUID_TAG));//B have uuid 
-        res = AStore.listResources("/");
-        Assert.assertFalse(res.contains(ResourceStore.METASTORE_UUID_TAG));//A did not
-
-        //try create again
-        AStore.createMetaStoreUuidIfNotExist();
-        BStore.createMetaStoreUuidIfNotExist();
-
-    }
 }
