e1b1c6636a08e40e125be10a9fd6bcec49d8ddec
Qian Xia <lauraxiaqian@gmail.com>
2020-04-26 17:26:12 +0800

    KE-13242 util index UT (#19096)

--- kystudio/src/util/__test__/index.spec.js
+++ kystudio/src/util/__test__/index.spec.js
@@ -1,4 +1,5 @@
 import * as util from '../index'
+import moment from 'moment-timezone'
 
 describe('util-index', () => {
   it('isEmptyObjectFun', () => {
@@ -221,5 +222,124 @@ describe('util-index', () => {
     const obj = {a: 1, b: 2}
     expect(util.objectToStr(obj)).toBe(JSON.stringify({a: 1, b: 2}))
     expect(util.objectToStr(1)).toBe('1')
+  })
+
+  it('getNextValInArray', () => {
+    const arr = ['a', 'b', 'c', 'd']
+    expect(util.getNextValInArray(arr, 'b')).toBe('c')
+    expect(util.getNextValInArray(arr, 'e')).toBe('a')
+    expect(util.getNextValInArray(arr, 'd')).toBe('a')
+  })
+
+  it('getNextOrPrevDate', () => {
+    expect(new Date(util.getNextOrPrevDate(1)).getDate() - new Date().getDate()).toBe(1)
+  })
+
+  it('isToday', () => {
+    expect(util.isToday(new Date().getTime())).toBeTruthy()
+    expect(util.isToday(util.getNextOrPrevDate(1))).not.toBeTruthy()
+  })
+
+  it('isThisWeek', () => {
+    expect(util.isThisWeek(util.getNextOrPrevDate(5))).toBeTruthy()
+    expect(util.isThisWeek(util.getNextOrPrevDate(8))).not.toBeTruthy()
+  })
+
+  it('isLastWeek', () => {
+    expect(util.isLastWeek(util.getNextOrPrevDate(-5))).toBeTruthy()
+    expect(util.isLastWeek(util.getNextOrPrevDate(8))).not.toBeTruthy()
+  })
+
+  it('utcToConfigTimeZone', () => {
+    const date = new Date().getTime()
+    const dateString = moment(date).format('YYYY-MM-DD HH:mm:ss')
+    expect(util.utcToConfigTimeZone(date, 'Asia/Shanghai')).toBe(dateString + ' GMT+8')
+  })
+
+  it('isObject', () => {
+    const obj = {}
+    const arr = []
+    expect(util.isObject(obj)).toBeTruthy()
+    expect(util.isObject(arr)).toBeTruthy()
+  })
+
+  it('looseEqual', () => {
+    const obj1 = {a: 8}
+    const obj2 = {a: 1}
+    const arr1 = [
+      {a: 1, b: 2},
+      {a: 1, b: 3},
+      {a: 0, b: 4}
+    ]
+    const arr2 = [
+      {a: 1, b: 2},
+      {a: 2, b: 3},
+      {a: 0, b: 4}
+    ]
+    expect(util.looseEqual(obj1, obj2)).not.toBeTruthy()
+    expect(util.looseEqual(arr1, arr2)).not.toBeTruthy()
+    expect(util.looseEqual('arr1', 'arr2')).not.toBeTruthy()
+  })
+
+  // it('handleSuccessAsync', () => {
+  //   const result = {
+  //     data: {
+  //       code: '000',
+  //       data: {
+  //         content: 'test',
+  //       },
+  //       msg: ''
+  //     }
+  //   }
+  //   let resultData = null
+  //   util.handleSuccessAsync(result)
+  //   expect(resultData).toEqual({"code": "000", "msg": "", "res": {"content": "test"}})
+  //   util.handleSuccessAsync(null, callback)
+  //   expect(resultData).toEqual({"res": null, "msg": "", "code": "000"})
+  //   expect(() => util.handleSuccessAsync(null, '')).toThrow()
+  // })
+
+  it('getFullMapping', () => {
+    const obj = {a: 1, b: 2}
+    expect(util.getFullMapping(obj)).toEqual({1: 'a', 2: 'b', a: 1, b: 2})
+  })
+
+  it('cacheSessionStorage', () => {
+    expect(util.cacheSessionStorage('test', 'abc')).toBe('abc')
+    sessionStorage.removeItem('test')
+  })
+
+  it('cacheLocalStorage', () => {
+    expect(util.cacheLocalStorage('test', 'abc')).toBe('abc')
+    localStorage.removeItem('test')
+  })
+
+  it('delayMs', async () => {
+    const tm = new Date().getTime()
+    await util.delayMs(2000)
+    const tm2 = new Date().getTime()
+    // 等待2000毫秒，其他执行时间不超过10毫秒
+    expect((tm2 - tm) - 2000 < 10).toBeTruthy()
+  })
+
+  it('filterInjectScript', () => {
+    const str1 = '<style>abc</style><script>def</script>xxxxxx'
+    const str2 = '<abcdef>'
+    expect(util.filterInjectScript(str1)).toBe('xxxxxx')
+    expect(util.filterInjectScript(str2)).toBe('&lt;abcdef&gt;')
+  })
+
+  it('camelToUnderline', () => {
+    const str = 'isAbc'
+    expect(util.camelToUnderline(str)).toBe('is_abc')
+  })
+
+  it('CamelToUnderlineForRequestParams', () => {
+    const json = {
+      isA: 'isA',
+      isB: 'isB',
+      isC: 'isC'
+    }
+    expect(util.CamelToUnderlineForRequestParams(json)).toEqual({is_a: 'isA', is_b: 'isB', is_c: 'isC'})
   })
 })
