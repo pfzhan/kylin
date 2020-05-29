a43948260cdfbe1a8c6e3306d516a062b9bd56e6
xm-c <57081674+xm-c@users.noreply.github.com>
2020-05-29 13:57:40 +0800

    KE-13344, change datatimerange text (#19355)
    Co-authored-by: Min Xu <min.xu@MinXudeMacBook-Pro.local>

--- kystudio/src/components/common/DropdownFilter/locales.js
+++ kystudio/src/components/common/DropdownFilter/locales.js
@@ -1,14 +1,14 @@
 export default {
   'en': {
     clearSelectItems: 'Clear selected items',
-    lastDay: 'Last Day',
-    lastWeek: 'Last Week',
-    lastMonth: 'Last Month'
+    lastDay: 'Last 1 day',
+    lastWeek: 'Last 7 days',
+    lastMonth: 'Last 30 days'
   },
   'zh-cn': {
     clearSelectItems: '清空所选项',
-    lastDay: '最近一天',
-    lastWeek: '最近一周',
-    lastMonth: '最近一月'
+    lastDay: '最近 1 天',
+    lastWeek: '最近 7 天',
+    lastMonth: '最近 30 天'
   }
 }
