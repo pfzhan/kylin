60e68ac42109fdb2d2f375ae76e2dfe34316caa6
Qian Xia <lauraxiaqian@gmail.com>
2020-08-03 15:17:18 +0800

    KE-13168 refine usergroup placeholder locals (#20435)

--- kystudio/src/components/common/GroupEditModal/locales.js
+++ kystudio/src/components/common/GroupEditModal/locales.js
@@ -4,13 +4,15 @@ export default {
     willCheckGroup: 'Unassigned Users',
     checkedGroup: 'Assigned Users',
     assignUser: 'Assign Users',
-    ok: 'OK'
+    ok: 'OK',
+    userGroupPld: 'Not support Chinese, nor special symbols (/:\\*?"<>|)'
   },
   'zh-cn': {
     createGroup: '新建组',
     willCheckGroup: '未分配的用户',
     checkedGroup: '已分配组用户',
     assignUser: '分配用户',
-    ok: '确定'
+    ok: '确定',
+    userGroupPld: '不支持中文和特殊字符（/:\\*?"<>|）'
   }
 }
