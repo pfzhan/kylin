270eb84dc9984081b574d590088fb0e4e6d150f9
xm-c <57081674+xm-c@users.noreply.github.com>
2020-08-10 17:34:25 +0800

    KE-16841, add expression tips (#20510)
    Co-authored-by: -l <min.xu@MinXudeMacBook-Pro.local>

--- kystudio/src/components/studio/StudioModel/ComputedColumnForm/ccform.vue
+++ kystudio/src/components/studio/StudioModel/ComputedColumnForm/ccform.vue
@@ -11,6 +11,10 @@
     </el-form-item>
     <el-form-item :label="$t('expression')" prop="expression" class="ksd-mb-10">
       <span slot="label">{{$t('kylinLang.dataSource.expression')}} <common-tip :content="$t('conditionExpress')" ><i class="el-icon-ksd-what"></i></common-tip></span>
+      <div class="expression-tips">
+        <p><span class="point">•</span><span class="text">{{$t('expressionTip1')}}</span></p>
+        <p><span class="point">•</span><span class="text">{{$t('expressionTip2')}}</span></p>
+      </div>
       <kap-editor ref="ccSql" height="100" lang="sql" theme="chrome" v-model="ccObject.expression" :read-only="!isEdit" @input="changeExpression">
       </kap-editor>
     </el-form-item>
@@ -64,7 +68,9 @@ import { NamedRegex } from 'config'
       requiredReturnType: 'The return type is required.',
       requiredExpress: 'The expression is required.',
       onlyStartLetters: 'Only supports starting with a letter',
-      deleteCCTip: 'After saving, as the expression was modified, measure(s) [{names}] would be unavailable and deleted. Do you want to continue?'
+      deleteCCTip: 'After saving, as the expression was modified, measure(s) [{names}] would be unavailable and deleted. Do you want to continue?',
+      expressionTip1: 'Within a project, the computed columns with same name must use identical expression',
+      expressionTip2: 'When referencing a column, please follow the format as TABLE_ALIAS.COLUMN (TABLE_ALIAS is the table name defined in the model)'
     },
     'zh-cn': {
       conditionExpress: '请注意，表达式中选用某列时，格式为“表名.列名”。',
@@ -80,7 +86,9 @@ import { NamedRegex } from 'config'
       requiredReturnType: '请选择返回类型',
       requiredExpress: '请输入表达式。',
       onlyStartLetters: '仅支持字母开头',
-      deleteCCTip: '修改后的可计算列表达式将导致度量 [{names}] 无法正常使用。保存后上述度量将被删除。确认要继续保存吗？'
+      deleteCCTip: '修改后的可计算列表达式将导致度量 [{names}] 无法正常使用。保存后上述度量将被删除。确认要继续保存吗？',
+      expressionTip1: '同一项目下同名的可计算列的表达式必须一致',
+      expressionTip2: '当引用列时，请使用 TABLE_ALIAS.COLUMN 格式来定义（TABLE_ALIAS 为模型中定义的表别名)'
     }
   }
 })
@@ -217,8 +225,8 @@ export default class CCForm extends Vue {
                 }
               }
               // 更改维度中引用该 cc 的 datatype
-              const index = this.modelInstance.dimensions.findIndex(it => it.name === cc.columnName)
-              index >= 0 && (this.modelInstance.dimensions[index].datatype = cc.datatype)
+              const index = this.modelInstance._mount.dimensions.findIndex(it => it.name === cc.columnName)
+              index >= 0 && this.modelInstance._mount.dimensions.splice(index, 1, {...this.modelInstance._mount.dimensions[index], datatype: cc.datatype})
               this.$emit('saveSuccess', cc)
               this.isEdit = false
             }, () => {
@@ -314,6 +322,25 @@ export default class CCForm extends Vue {
       text-overflow: ellipsis;
       position: absolute;
       margin-left: 5px;
+    }
+    .expression-tips {
+      background: @regular-background-color;
+      font-size: 12px;
+      padding: 15px 10px;
+      box-sizing: border-box;
+      margin-bottom: 10px;
+      p {
+        line-height: 20px;
+      }
+      .point {
+        margin-right: 10px;
+        vertical-align: top;
+      }
+      .text {
+        display: inline-block;
+        width: calc(~'100% - 30px');
+        word-break: break-all;
+      }
     }
   }
 </style>
