79284cf5fe0eba6433b383c49245ac9c74bef88e
Pengfei Zhan <pengfei.zhan@kyligence.io>
2021-09-16 11:06:41 +0800

    KE-30815 support adjust cache size & cache duration

--- src/core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
+++ src/core-common/src/main/java/org/apache/kylin/common/KylinConfigBase.java
@@ -519,11 +519,11 @@ public abstract class KylinConfigBase implements Serializable {
     }
 
     public int getMetadataCacheMaxNum() {
-        return Integer.parseInt(getOptional("kylin.metadata.cache.max-num", String.valueOf(Integer.MAX_VALUE)));
+        return Integer.parseInt(getOptional("kylin.metadata.cache.max-num", "100"));
     }
 
     public int getMetadataCacheMaxDuration() {
-        return Integer.parseInt(getOptional("kylin.metadata.cache.max-duration", String.valueOf(Integer.MAX_VALUE)));
+        return Integer.parseInt(getOptional("kylin.metadata.cache.max-duration", "300"));
     }
 
     public boolean skipRecordJobExecutionTime() {
