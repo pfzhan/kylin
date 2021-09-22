1d80110306313477318e3751989874e79d4d28ff
JianHuangKylin <85476894+JianHuangKylin@users.noreply.github.com>
2021-09-22 16:57:07 +0800

    KC-8312: snowflake aad ut

--- src/datasource-sdk/src/test/java/org/apache/kylin/sdk/datasource/adaptor/AdaptorsTest.java
+++ src/datasource-sdk/src/test/java/org/apache/kylin/sdk/datasource/adaptor/AdaptorsTest.java
@@ -23,14 +23,48 @@
  */
 package org.apache.kylin.sdk.datasource.adaptor;
 
-import java.sql.SQLException;
+import static org.powermock.api.mockito.PowerMockito.when;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicStatusLine;
+import org.apache.kylin.common.KylinConfigExt;
+import org.apache.kylin.common.util.JsonUtil;
+import org.apache.kylin.sdk.datasource.adaptor.response.KylinSnowflakeResponse;
 import org.apache.kylin.sdk.datasource.framework.JdbcConnectorTest;
 import org.apache.kylin.sdk.datasource.framework.SourceConnectorFactory;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import lombok.SneakyThrows;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({HttpClients.class, UserGroupInformation.class})
 public class AdaptorsTest extends JdbcConnectorTest {
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        PowerMockito.mockStatic(UserGroupInformation.class);
+        UserGroupInformation userGroupInformation = Mockito.mock(UserGroupInformation.class);
+        PowerMockito.when(UserGroupInformation.getCurrentUser()).thenReturn(userGroupInformation);
+    }
 
     @Test
     public void testMySQLAdaptor() throws Exception {
@@ -63,6 +97,52 @@ public class AdaptorsTest extends JdbcConnectorTest {
         getTestConfig().setProperty("kylin.source.jdbc.adaptor", "org.apache.kylin.sdk.datasource.adaptor.SnowflakeAdaptor");
         getTestConfig().setProperty("kylin.source.jdbc.dialect", "snowflake");
         connector = SourceConnectorFactory.getJdbcConnector(getTestConfig());
+        AbstractJdbcAdaptor adaptor = connector.getAdaptor();
+        Assert.assertEquals("select 1", adaptor.fixSql("select 1"));
+        Assert.assertFalse(connector.listDatabases().isEmpty());
+        Assert.assertFalse(connector.listTables("DEFAULT").isEmpty());
+        Assert.assertNotNull(connector.listColumns("DB", "ROLES"));
+    }
+
+    @Test
+    public void testSnowflakeTokenAdaptor() throws SQLException, IOException {
+        PowerMockito.mockStatic(UserGroupInformation.class);
+        UserGroupInformation userGroupInformation = Mockito.mock(UserGroupInformation.class);
+        PowerMockito.when(UserGroupInformation.getCurrentUser()).thenReturn(userGroupInformation);
+
+        getTestConfig().setProperty("kylin.source.jdbc.adaptor", "org.apache.kylin.sdk.datasource.adaptor.SnowflakeAdaptor");
+        getTestConfig().setProperty("kylin.source.jdbc.dialect", "snowflake");
+
+        Map<String, String> options = new HashMap<>();
+        options.put("snowflake.oauth.enabled", "true");
+        options.put("kylin.source.authenticator", "?");
+        options.put(SnowflakeAdaptor.OAUTH_TOKEN_URL, "https://login.microsoftonline.com/111/oauth2/v2.0/token");
+        options.put(SnowflakeAdaptor.AAD_CLIENT_ID, "73918259-8af4-46b0-a514-1116");
+        options.put(SnowflakeAdaptor.AAD_CLIENT_SECRET, "11111");
+        options.put(SnowflakeAdaptor.AAD_SCOPE, "https://8b66b074-2767-4f10-111/.default");
+        options.put(SnowflakeAdaptor.AAD_GRANT_TYPE, "client_credentials");
+
+        PowerMockito.mockStatic(HttpClients.class);
+        CloseableHttpClient httpClient = Mockito.mock(CloseableHttpClient.class);
+        CloseableHttpResponse httpResponse = Mockito.mock(CloseableHttpResponse.class);
+
+        when(httpClient.execute(Mockito.any())).thenReturn(httpResponse);
+
+        when(httpResponse.getStatusLine()).thenReturn(new BasicStatusLine(HttpVersion.HTTP_1_1, 200, ""));
+        final KylinSnowflakeResponse snowflakeResponse = new KylinSnowflakeResponse();
+        snowflakeResponse.setAccessToken("111111");
+        when(httpResponse.getEntity()).thenReturn(new BasicHttpEntity() {
+            @SneakyThrows
+            @Override
+            public InputStream getContent() throws IllegalStateException {
+                return new ByteArrayInputStream(JsonUtil.writeValueAsIndentBytes(snowflakeResponse));
+            }
+        });
+        when(HttpClients.createDefault()).thenReturn(httpClient);
+
+
+        KylinConfigExt kylinConfigExt = KylinConfigExt.createInstance(getTestConfig(), options);
+        connector = SourceConnectorFactory.getJdbcConnector(kylinConfigExt);
         AbstractJdbcAdaptor adaptor = connector.getAdaptor();
         Assert.assertEquals("select 1", adaptor.fixSql("select 1"));
         Assert.assertFalse(connector.listDatabases().isEmpty());
