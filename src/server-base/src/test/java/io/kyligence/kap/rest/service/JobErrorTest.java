2823ae96dd033d6575263033ee79c1a3cff25526
Zhixiong Chen <chen@apache.org>
2021-12-10 11:13:28 +0800

    fix ut (#26245)

--- src/server-base/src/test/java/io/kyligence/kap/rest/service/JobErrorTest.java
+++ src/server-base/src/test/java/io/kyligence/kap/rest/service/JobErrorTest.java
@@ -60,7 +60,6 @@ import org.apache.spark.application.NoRetryException;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
