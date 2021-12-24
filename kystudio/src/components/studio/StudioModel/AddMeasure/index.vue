42073896702f9f7ae6478d25be225be7da6851a4
xm-c <57081674+xm-c@users.noreply.github.com>
2021-12-24 19:20:42 +0800

    KE-33102, reset code (#26482)

--- kystudio/src/components/studio/StudioModel/AddMeasure/index.vue
+++ kystudio/src/components/studio/StudioModel/AddMeasure/index.vue
@@ -110,7 +110,7 @@
               </el-option>
               <el-option-group key="ccolumn" :label="$t('ccolumns')" v-if="measure.expression === 'TOP_N' && getCCGroups.length">
                 <el-option
-                  v-for="item in getCCGroups(isGroupBy)"
+                  v-for="item in getCCGroups"
                   :key="item.guid"
                   :label="item.tableAlias + '.' + item.columnName"
                   :value="item.tableAlias + '.' + item.columnName">
@@ -380,16 +380,10 @@ export default class AddMeasure extends Vue {
       expression: [{ required: true, message: this.$t('requiredExpress'), trigger: 'change' }]
     }
   }
-  getCCGroups (isGroupBy) {
+  get getCCGroups () {
     if (this.ccGroups.length) {
-      if (['SUM(column)', 'CORR'].includes(this.measure.expression)) {
+      if (['SUM(column)', 'TOP_N', 'CORR'].includes(this.measure.expression)) {
         return this.ccGroups.filter(it => measureSumAndTopNDataType.includes(it.datatype.toLocaleLowerCase().match(/^(\w+)\(?/)[1]))
-      } else if (this.measure.expression === 'TOP_N') {
-        if (isGroupBy && isGroupBy === 'Group by') {
-          return this.ccGroups.filter(it => measuresDataType.includes(it.datatype.toLocaleLowerCase().match(/^(\w+)\(?/)[1]))
-        } else {
-          return this.ccGroups.filter(it => measureSumAndTopNDataType.includes(it.datatype.toLocaleLowerCase().match(/^(\w+)\(?/)[1]))
-        }
       } else if (this.measure.expression === 'PERCENTILE_APPROX') {
         return this.ccGroups.filter(item => measurePercenDataType.includes(item.datatype.toLocaleLowerCase().match(/^(\w+)\(?/)[1]))
       } else {
