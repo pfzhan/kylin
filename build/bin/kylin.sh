d8838e5b9d4d48df31f84725a889d86c6ce78406
SibingZhang <74443791+SibingZhang@users.noreply.github.com>
2022-03-07 19:30:37 +0800

    AL-5367 The disk is overloaded because there is no automatic cleaning policy for kylin.out (#27238)

--- build/bin/kylin.sh
+++ build/bin/kylin.sh
@@ -28,7 +28,6 @@ source $(cd -P -- "$(dirname -- "$0")" && pwd -P)/../sbin/header.sh "$@"
 mkdir -p ${KYLIN_HOME}/logs
 ERR_LOG=${KYLIN_HOME}/logs/shell.stderr
 OUT_LOG=${KYLIN_HOME}/logs/shell.stdout
-${KYLIN_HOME}/sbin/rotate-logs.sh "$@"
 echo "-----------------------  log start  -----------------------" >>${ERR_LOG}
 echo "-----------------------  log start  -----------------------" >>${OUT_LOG}
 bash -x ${KYLIN_HOME}/sbin/bootstrap.sh "$@" 2>>${ERR_LOG}  | tee -a ${OUT_LOG}
