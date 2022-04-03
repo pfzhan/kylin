12bece02d927e7955266758742372bab55fd2b03
fenghuang <8199608+huangfeng1993@users.noreply.github.com>
2022-04-03 18:58:52 +0800

    KE-35548 init epoch after register epochChangedListener (#27470)
    * init epoch after register epochChangedListener

--- src/yinglong-common-service/src/main/java/io/kyligence/kap/rest/config/AppInitializer.java
+++ src/yinglong-common-service/src/main/java/io/kyligence/kap/rest/config/AppInitializer.java
@@ -150,11 +150,14 @@ public class AppInitializer {
     @EventListener(ApplicationReadyEvent.class)
     public void afterReady(ApplicationReadyEvent event) {
         val kylinConfig = KylinConfig.getInstanceFromEnv();
+
+        if (kylinConfig.isJobNode()) {
+            new EpochOrchestrator(kylinConfig);
+        }
         if (kylinConfig.getJStackDumpTaskEnabled()) {
             taskScheduler.scheduleAtFixedRate(new JStackDumpTask(),
                     kylinConfig.getJStackDumpTaskPeriod() * Constant.MINUTE);
         }
-
         if (kylinConfig.isGuardianEnabled() && kylinConfig.isGuardianHAEnabled()) {
             log.info("Guardian Process ha is enabled, start check scheduler");
             taskScheduler.scheduleAtFixedRate(new KapGuardianHATask(),
