146cb86aa7eb15cdd1dd9a27c7caa3527b9f5cfe
Yinghao Lin <39019287+yhcast0@users.noreply.github.com>
2022-04-19 10:05:15 +0800

    KE-35000 Update: Fix missing error code. 0418 (#27580)

--- src/yinglong-smart-server/src/main/java/io/kyligence/kap/rest/controller/open/OpenModelSmartController.java
+++ src/yinglong-smart-server/src/main/java/io/kyligence/kap/rest/controller/open/OpenModelSmartController.java
@@ -26,8 +26,8 @@ package io.kyligence.kap.rest.controller.open;
 
 import static io.kyligence.kap.common.constant.HttpConstant.HTTP_VND_APACHE_KYLIN_V4_PUBLIC_JSON;
 import static org.apache.kylin.common.exception.ServerErrorCode.EMPTY_SQL_EXPRESSION;
-import static org.apache.kylin.common.exception.ServerErrorCode.MODEL_NOT_EXIST;
 import static org.apache.kylin.common.exception.code.ErrorCodeServer.MODEL_NAME_EMPTY;
+import static org.apache.kylin.common.exception.code.ErrorCodeServer.MODEL_NAME_NOT_EXIST;
 
 import java.util.List;
 import java.util.Locale;
@@ -257,8 +257,7 @@ public class OpenModelSmartController extends NBasicController {
                 .findFirst().orElse(null);
 
         if (model == null) {
-            throw new KylinException(MODEL_NOT_EXIST,
-                    String.format(Locale.ROOT, MsgPicker.getMsg().getMODEL_NOT_FOUND(), modelAlias));
+            throw new KylinException(MODEL_NAME_NOT_EXIST, modelAlias);
         }
         if (model.isBroken()) {
             throw new KylinException(ServerErrorCode.MODEL_BROKEN,
