9805de8a8b32b00ada73032595dfe3ff19fbfead
Feng Zhu <fishcus@outlook.com>
2023-11-17 14:26:11 +0800

    KE-42889 modify 'model_id' column length for job_info table (#31260)

--- src/core-job/src/main/resources/script/schema_job_info_h2.sql
+++ src/core-job/src/main/resources/script/schema_job_info_h2.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS KE_IDENTIFIED_job_info (
   job_status varchar(50) NOT NULL,
   project varchar(100) NOT NULL,
   subject varchar(200) NOT NULL,
-  model_id varchar(100),
+  model_id varchar(200),
   priority integer DEFAULT 3,
   job_content longblob NOT NULL,
   mvcc bigint(10),
